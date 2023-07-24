ab7d64434c91dbbab1d17e816eafbc27c26b4c7e
moonn6pence <shin421179@gmail.com>
2023-07-24 15:56:30 +0900

    feat : controlleradvice 추가
    - 속닥속닥 방식 차용

--- backend/jansori/src/main/java/com/a606/jansori/JansoriApplication.java
+++ backend/jansori/src/main/java/com/a606/jansori/JansoriApplication.java
@@ -3,7 +3,6 @@ package com.a606.jansori;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class JansoriApplication {
---
+++ backend/jansori/src/main/java/com/a606/jansori/global/exception/advice/ExceptionControllerAdvice.java
@@ -0,0 +1,93 @@
+package com.a606.jansori.global.exception.advice;
+
+import com.a606.jansori.global.common.EnvelopeResponse;
+import com.a606.jansori.global.exception.ForbiddenException;
+import com.a606.jansori.global.exception.NotFoundException;
+import com.a606.jansori.global.exception.UnauthorizedException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ExceptionControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public EnvelopeResponse BadRequestExceptionHandler(MethodArgumentNotValidException e) {
+        log.error(e.getMessage());
+
+        StringBuilder errorMessage = new StringBuilder();
+        e.getBindingResult().getAllErrors().forEach((error) -> {
+            errorMessage.append(error.getDefaultMessage());
+        });
+
+        return EnvelopeResponse.builder()
+                .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+                .message(errorMessage.toString())
+                .build();
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public EnvelopeResponse RuntimeExceptionHandler(RuntimeException e) {
+
+        log.error(e.getStackTrace().toString());
+
+        return EnvelopeResponse.builder()
+                .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+                .message(HttpStatus.INTERNAL_SERVER_ERROR.name())
+                .build();
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public EnvelopeResponse NotFoundExceptionHandler(NotFoundException e) {
+
+        log.error(e.getStackTrace().toString());
+
+        return EnvelopeResponse.builder()
+                .code(e.getCode())
+                .message(e.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public EnvelopeResponse BadRequestExceptionHandler(NotFoundException e) {
+
+        log.error(e.getStackTrace().toString());
+
+        return EnvelopeResponse.builder()
+                .code(e.getCode())
+                .message(e.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public EnvelopeResponse UnauthorizedExceptionHandler(UnauthorizedException e) {
+
+        log.error(e.getStackTrace().toString());
+
+        return EnvelopeResponse.builder()
+                .code(e.getCode())
+                .message(e.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(ForbiddenException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public EnvelopeResponse ForbiddenExceptionHandler(ForbiddenException e) {
+
+        log.error(e.getStackTrace().toString());
+
+        return EnvelopeResponse.builder()
+                .code(e.getCode())
+                .message(e.getMessage())
+                .build();
+    }
+}
