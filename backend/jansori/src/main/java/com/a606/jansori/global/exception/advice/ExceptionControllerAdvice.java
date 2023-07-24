75715cb8738c460a45d4d1c385e1932e50692f5d
moonn6pence <shin421179@gmail.com>
2023-07-24 17:19:10 +0900

    feat : binding result 사용
    - BindException의 특성 이용

--- backend/jansori/src/main/java/com/a606/jansori/global/exception/advice/ExceptionControllerAdvice.java
+++ backend/jansori/src/main/java/com/a606/jansori/global/exception/advice/ExceptionControllerAdvice.java
@@ -1,11 +1,13 @@
 package com.a606.jansori.global.exception.advice;
 
 import com.a606.jansori.global.common.EnvelopeResponse;
+import com.a606.jansori.global.exception.BadRequestException;
 import com.a606.jansori.global.exception.ForbiddenException;
 import com.a606.jansori.global.exception.NotFoundException;
 import com.a606.jansori.global.exception.UnauthorizedException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -17,13 +19,15 @@ public class ExceptionControllerAdvice {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public EnvelopeResponse BadRequestExceptionHandler(MethodArgumentNotValidException e) {
-        log.error(e.getMessage());
+    public EnvelopeResponse BadRequestExceptionHandler(BindingResult bindingResult) {
 
         StringBuilder errorMessage = new StringBuilder();
-        e.getBindingResult().getAllErrors().forEach((error) -> {
+
+        bindingResult.getAllErrors().forEach((error) -> {
             errorMessage.append(error.getDefaultMessage());
         });
+
+        log.error(errorMessage.toString());
 
         return EnvelopeResponse.builder()
                 .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
@@ -57,7 +61,7 @@ public class ExceptionControllerAdvice {
 
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public EnvelopeResponse BadRequestExceptionHandler(NotFoundException e) {
+    public EnvelopeResponse BadRequestExceptionHandler(BadRequestException e) {
 
         log.error(e.getStackTrace().toString());
 
