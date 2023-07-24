25eeb83182b97836dcf8f756aab3db74b1be593f
Bae Young Suk <bysuk01@hanyang.ac.kr>
2023-07-24 17:53:46 +0900

    feat: 구글 로그인 구현

--- backend/jansori/src/main/java/com/a606/jansori/global/oauth/OAuthAttributes.java
+++ backend/jansori/src/main/java/com/a606/jansori/global/oauth/OAuthAttributes.java
@@ -2,7 +2,31 @@ package com.a606.jansori.global.oauth;
 
 import com.a606.jansori.domain.member.domain.Member;
 
-public enum OAuthAttributes {
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
 
+public enum OAuthAttributes {
+    GOOGLE("google", (attributes) -> {
+        Member member = new Member();
+        return member;
+    });
+
+
+    private final String registrationId;
+    private final Function<Map<String, Object>, Member> of;
+
+    OAuthAttributes(String registrationId, Function<Map<String, Object>, Member> of) {
+        this.registrationId = registrationId;
+        this.of = of;
+    }
+
+    public static Member extract(String registrationId, Map<String, Object> attributes) {
+        return Arrays.stream(values())
+                .filter(provider -> registrationId.equals(provider.registrationId))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new)
+                .of.apply(attributes);
+    }
 
 }
