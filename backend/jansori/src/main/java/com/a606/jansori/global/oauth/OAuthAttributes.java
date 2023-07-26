8a294714fd09eff80f9fbfcfab5c896093dfaf5b
Bae Young Suk <bysuk01@hanyang.ac.kr>
2023-07-26 11:22:13 +0900

    fix: 리베이스 충돌 해결

--- backend/jansori/src/main/java/com/a606/jansori/global/oauth/OAuthAttributes.java
+++ backend/jansori/src/main/java/com/a606/jansori/global/oauth/OAuthAttributes.java
@@ -1,2 +1,8 @@
-package com.a606.jansori.global.oauth;public enum OAuthAttributes {
+package com.a606.jansori.global.oauth;
+
+import com.a606.jansori.domain.member.domain.Member;
+
+public enum OAuthAttributes {
+
+
 }
