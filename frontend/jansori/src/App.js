3f2280b437bd235ed6ed15f5a48e93197ceba7ef
lcj000107 <lcj000107@naver.com>
2023-07-26 17:41:45 +0900

    remove: App.js 파일과 index.jsx 파일 내용 삭제

--- frontend/jansori/src/App.js
+++ frontend/jansori/src/App.js
@@ -1,15 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import LoginPage from "./pages/LoginPage";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-      </Routes>
-    </Router>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- frontend/jansori/src/pages/LoginPage/index.jsx
+++ frontend/jansori/src/pages/LoginPage/index.jsx
@@ -1,13 +1,7 @@
 import React from "react";
-import Button from "../../components/UI/Button";
 
 const LoginPage = () => {
-  return (
-    <div>
-      <Button label={"완료"} normal />
-      <Button label={"취소"} cancel />
-    </div>
-  );
+  return <></>;
 };
 
 export default LoginPage;
