e82c8b3624246dc59767ade4718c47b4a7816eff
신현철 <shin421179@gmail.com>
2023-07-26 10:45:17 +0900

    feat : api 완성
    - 지연로딩 적용
    - post 작성 메소드 연관관계 편의 메소드로 수정
    
    * refactor : validation 추가, 정적팩토리메소드 수정
    
    - Todo content Length validation
    - Todo 리턴하는 팩토리 메소드 위치를 엔티티에서 Dto로 수정
    
    * feat : dto 팩토리 메서드 추가

--- backend/jansori/src/main/java/com/a606/jansori/domain/todo/repository/TodoRepository.java
+++ backend/jansori/src/main/java/com/a606/jansori/domain/todo/repository/TodoRepository.java
@@ -1,7 +1,13 @@
 package com.a606.jansori.domain.todo.repository;
 
+import com.a606.jansori.domain.member.domain.Member;
 import com.a606.jansori.domain.todo.domain.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+    List<Todo> findAllByMemberAndCreatedAtBetween(Member member, LocalDateTime today, LocalDateTime tomorrow);
 }
