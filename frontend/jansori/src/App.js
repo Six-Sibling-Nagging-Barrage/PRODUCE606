ce3217211a649a18fddd191f9dde3a65afcdcbab
lcj000107 <lcj000107@naver.com>
2023-07-26 17:35:53 +0900

    feat: 공통 컴포넌트 Button 기능 구현 완료

--- frontend/jansori/src/App.js
+++ frontend/jansori/src/App.js
@@ -1,7 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ frontend/jansori/src/pages/LoginPage/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "../../components/UI/Button";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <Button label={"완료"} normal />
+      <Button label={"취소"} cancel />
+    </div>
+  );
+};
+
+export default LoginPage;
