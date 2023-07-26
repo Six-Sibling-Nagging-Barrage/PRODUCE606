ace8df072391ddbef99e71f3136843fbf1258426
Bae Young Suk <bysuk01@hanyang.ac.kr>
2023-07-26 11:22:13 +0900

    fix: 리베이스 충돌 해결

--- backend/jansori/src/main/java/com/a606/jansori/domain/member/domain/MemberRepository.java
+++ backend/jansori/src/main/java/com/a606/jansori/domain/member/domain/MemberRepository.java
@@ -1,2 +1,11 @@
-package com.a606.jansori.domain.member.domain;public interface MemberRepository {
+package com.a606.jansori.domain.member.domain;
+
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<User> findByEmail(String email); //email과 pid로?
+
 }
