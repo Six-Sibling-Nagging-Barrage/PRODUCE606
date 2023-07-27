2d08724d4a28f72e75f9b38e5fa091c0df84da68
moonn6pence <shin421179@gmail.com>
2023-07-27 09:50:10 +0900

    feat : DB 수정으로 TodoMetadata 엔티티 추가

---
+++ backend/jansori/src/main/java/com/a606/jansori/domain/todo/domain/TodoMetadata.java
@@ -0,0 +1,30 @@
+package com.a606.jansori.domain.todo.domain;
+
+import com.a606.jansori.domain.nag.domain.Nag;
+import com.a606.jansori.domain.persona.domain.Persona;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class TodoMetadata {
+
+    @Id
+    @Column(name = "todo_metadata_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "persona_id")
+    private Persona persona;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "nag_id")
+    private Nag nag;
+
+    @Column
+    private Long count;
+}
