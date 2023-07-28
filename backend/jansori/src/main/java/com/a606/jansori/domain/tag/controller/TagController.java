ac3cc76f163e5072c9bfde4af1b3f18e6d874642
taeyong <amico741@naver.com>
2023-07-28 17:44:46 +0900

    feat: member가 구독하고 있는 태그 리스트들을 가져오는 비즈니스 로직 추가

--- backend/jansori/src/main/java/com/a606/jansori/domain/tag/controller/TagController.java
+++ backend/jansori/src/main/java/com/a606/jansori/domain/tag/controller/TagController.java
@@ -1,8 +1,12 @@
 package com.a606.jansori.domain.tag.controller;
 
+import com.a606.jansori.domain.tag.dto.GetFollowingTagResDto;
+import com.a606.jansori.domain.tag.dto.TagElement;
 import com.a606.jansori.domain.tag.service.TagService;
 import com.a606.jansori.global.common.EnvelopeResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Slice;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +23,13 @@ public class TagController {
   public EnvelopeResponse followTagByTagWithMember(Long memberId, @PathVariable Long tagId) {
     tagService.followTagByTagWithMember(memberId, tagId);
     return EnvelopeResponse.builder().build();
+  }
+
+  @GetMapping("/{memberId}/list")
+  public EnvelopeResponse<GetFollowingTagResDto> getFollowingTagByMemberId(
+      @PathVariable Long memberId) {
+    return EnvelopeResponse.<GetFollowingTagResDto>builder()
+        .data(tagService.getFollowingTagByMemberId(memberId))
+        .build();
   }
 }
--- backend/jansori/src/main/java/com/a606/jansori/domain/tag/service/TagService.java
+++ backend/jansori/src/main/java/com/a606/jansori/domain/tag/service/TagService.java
@@ -5,15 +5,22 @@ import com.a606.jansori.domain.member.exception.MemberNotFoundException;
 import com.a606.jansori.domain.member.repository.MemberRepository;
 import com.a606.jansori.domain.tag.domain.Tag;
 import com.a606.jansori.domain.tag.domain.TagFollow;
+import com.a606.jansori.domain.tag.dto.GetFollowingTagResDto;
+import com.a606.jansori.domain.tag.dto.TagElement;
 import com.a606.jansori.domain.tag.exception.TagNotFoundException;
 import com.a606.jansori.domain.tag.repository.TagFollowRepository;
 import com.a606.jansori.domain.tag.repository.TagRepository;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class TagService {
@@ -30,6 +37,15 @@ public class TagService {
     Optional<TagFollow> tagFollow = tagFollowRepository.findTagFollowByTagAndMember(tag, member);
 
     tagFollow.ifPresentOrElse(tagFollowRepository::delete,
-            () -> tagFollowRepository.save(TagFollow.builder().tag(tag).member(member).build()));
+        () -> tagFollowRepository.save(TagFollow.builder().tag(tag).member(member).build()));
+  }
+
+  @Transactional(readOnly = true)
+  public GetFollowingTagResDto getFollowingTagByMemberId(Long memberId) {
+    Member member = memberRepository.findById(memberId).orElseThrow(MemberNotFoundException::new);
+
+    List<TagFollow> tagFollows = tagFollowRepository.findTagFollowsByMember(member);
+    return GetFollowingTagResDto.from(
+        tagFollows.stream().map(TagElement::new).collect(Collectors.toList()));
   }
 }
