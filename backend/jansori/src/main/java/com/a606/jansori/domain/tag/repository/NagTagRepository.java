6600c1b71338971fcbb1fc2beaa9c60537bb41ad
moonn6pence <shin421179@gmail.com>
2023-07-28 14:36:55 +0900

    feat : 잔소리 랜덤 추출을 위한 쿼리 추가

--- backend/jansori/src/main/java/com/a606/jansori/domain/tag/repository/NagTagRepository.java
+++ backend/jansori/src/main/java/com/a606/jansori/domain/tag/repository/NagTagRepository.java
@@ -1,8 +1,21 @@
 package com.a606.jansori.domain.tag.repository;
 
+import com.a606.jansori.domain.member.domain.Member;
 import com.a606.jansori.domain.tag.domain.NagTag;
+import com.a606.jansori.domain.tag.domain.Tag;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface NagTagRepository extends JpaRepository<NagTag, Long> {
+
+  Long countDistinctByNag_MemberNotAndTagIn(Member member, List<Tag> tags);
+
+  @Query(value = "select nt from nag_tag nt "
+      + "join fetch nag n "
+      + "join fetch tag t "
+      + "where t in :tags ")
+  List<NagTag> findByNag_MemberNotAndTagIn(Member member, List<Tag> tags, Pageable pageable);
 
 }
