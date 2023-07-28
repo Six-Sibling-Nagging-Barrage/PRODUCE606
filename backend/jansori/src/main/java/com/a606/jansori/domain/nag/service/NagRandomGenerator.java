3c58f49f4256160d84ca7cd146d50308609ab869
moonn6pence <shin421179@gmail.com>
2023-07-28 14:35:31 +0900

    feat : 잔소리 랜덤 추출 객체 메소드 추가

--- backend/jansori/src/main/java/com/a606/jansori/domain/nag/service/NagRandomGenerator.java
+++ backend/jansori/src/main/java/com/a606/jansori/domain/nag/service/NagRandomGenerator.java
@@ -1,14 +1,44 @@
 package com.a606.jansori.domain.nag.service;
 
+import com.a606.jansori.domain.member.domain.Member;
+import com.a606.jansori.domain.nag.domain.Nag;
+import com.a606.jansori.domain.persona.domain.Line;
 import com.a606.jansori.domain.persona.domain.Persona;
+import com.a606.jansori.domain.tag.domain.Tag;
 import com.a606.jansori.domain.tag.domain.TodoTag;
+import com.a606.jansori.domain.tag.repository.NagTagRepository;
+import com.a606.jansori.domain.tag.repository.TodoTagRepository;
+import com.a606.jansori.global.util.RandomUtil;
 import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class NagRandomGenerator {
 
-  public void getRandomLineOfPersona(Persona persona) {
+  private final NagTagRepository nagTagRepository;
+
+  private final RandomUtil randomUtil;
+
+  public Line getRandomLineOfPersona(Member member, Persona persona) {
+    return null;
   }
 
-  public void getRandomNagOfUser(List<TodoTag> todoTags) {
+  public Nag getRandomNagWithTags(Member member, List<TodoTag> todoTags) {
+
+    List<Tag> tags = todoTags.stream().map(TodoTag::getTag).collect(Collectors.toList());
+
+    Long count = nagTagRepository.countDistinctByNag_MemberNotAndTagIn(member, tags);
+
+    int randomIndex = randomUtil.generate(count);
+
+    return nagTagRepository.findByNag_MemberNotAndTagIn(member,
+            tags,
+            PageRequest.of(randomIndex, 1))
+        .get(0)
+        .getNag();
   }
 }
