cc052d259dbd2ee8a1ca2a05412422cebbffee71
taeyong <amico741@naver.com>
2023-07-28 12:27:42 +0900

    feat: 잔소리 랜덤 생성을 위한 클래스 추가

---
+++ backend/jansori/src/main/java/com/a606/jansori/domain/tag/service/NagRandomGenerator.java
@@ -0,0 +1,14 @@
+package com.a606.jansori.domain.tag.service;
+
+import com.a606.jansori.domain.persona.domain.Persona;
+import com.a606.jansori.domain.tag.domain.TodoTag;
+import java.util.List;
+
+public class NagRandomGenerator {
+
+  public void getRandomLineOfPersona(Persona persona) {
+  }
+
+  public void getRandomNag(List<TodoTag> todoTags) {
+  }
+}
