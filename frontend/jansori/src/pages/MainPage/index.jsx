98509979bf6e0dae0578f7ad4b1fe0d59a6df31d
이채정 <lcj000107@naver.com>
2023-07-30 02:22:51 +0900

    design: 메인 페이지 배경 그라데이션 화면 디자인 완성

--- frontend/jansori/src/pages/MainPage/index.jsx
+++ frontend/jansori/src/pages/MainPage/index.jsx
@@ -1,12 +1,11 @@
-import React from "react";
-import styled from "styled-components";
-import tw from "twin.macro";
-import StartButton from "./components/StartButton";
+import React from 'react';
+import tw, { styled } from 'twin.macro';
+import StartButton from './components/StartButton';
 
 const MainPage = () => {
   return (
     <BackgroundStyled>
-      <StartButton nagCount={"172032"} />
+      <StartButton nagCount={'172032'} />
     </BackgroundStyled>
   );
 };
@@ -16,7 +15,20 @@ export default MainPage;
 const BackgroundStyled = styled.div`
   ${tw`
   min-h-screen
-  bg-gray-300
   snap-x
 `}
+  background: linear-gradient(-45deg, rgba(36, 45, 66, 0.15), rgba(173, 0, 255, 0.35), rgba(113, 84, 255, 0.4), rgba(0, 148, 255, 0.4), rgba(136, 118, 247, 0.4));
+  background-size: 400% 400%;
+  animation: gradient 15s ease infinite;
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
 `;
