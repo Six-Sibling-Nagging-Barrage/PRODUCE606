8c7f0d25029636dfe6854c54b1057dbb930c4eb7
김태용 <amico741@naver.com>
2023-07-29 16:07:38 +0900

    fix: 제거된 메서드 추가

--- backend/jansori/src/main/java/com/a606/jansori/domain/tag/repository/TagFollowRepository.java
+++ backend/jansori/src/main/java/com/a606/jansori/domain/tag/repository/TagFollowRepository.java
@@ -4,17 +4,17 @@ import com.a606.jansori.domain.member.domain.Member;
 import com.a606.jansori.domain.tag.domain.Tag;
 import com.a606.jansori.domain.tag.domain.TagFollow;
 import java.util.List;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface TagFollowRepository extends JpaRepository<TagFollow, Long> {
 
+  Optional<TagFollow> findTagFollowByTagAndMember(Tag tag, Member member);
 
   @Query("select tf from tag_follow as tf left join fetch tf.tag")
   List<TagFollow> findTagFollowsByMember(Member member);
+
+  @Query(value = "select tf from tag_follow tf join fetch tf.tag tg join fetch tf.member m where tf.member = :member")
+  List<TagFollow> findAllByMember(Member member);
 }
