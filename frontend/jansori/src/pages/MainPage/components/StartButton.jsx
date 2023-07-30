3f022852b16d930a83a3aef319031571b56c474a
이채정 <lcj000107@naver.com>
2023-07-30 14:59:26 +0900

    feat: 잔소리 버튼하기 화면 구현

--- frontend/jansori/src/pages/MainPage/components/StartButton.jsx
+++ frontend/jansori/src/pages/MainPage/components/StartButton.jsx
@@ -1,42 +1,49 @@
-import React from "react";
-import tw, { styled } from "twin.macro";
+import React from 'react';
+import tw, { styled } from 'twin.macro';
 
 const StartButton = (props) => {
   const { nagCount } = props;
   return (
-    <SpeechBubblt>
+    <SpeechBubble>
       <BubbleSpeech>
         <p>잔소리 하러 가기</p>
       </BubbleSpeech>
       <p>현재 작성된 잔소리 총 {nagCount}개</p>
-    </SpeechBubblt>
+    </SpeechBubble>
   );
 };
 
 export default StartButton;
 
-const SpeechBubblt = styled.div`
+const SpeechBubble = styled.div`
   ${tw`
-  snap-center
-  w-6/12
-  h-fit
+  w-2/3
+  sm:w-1/3
+  md:w-3/5
+  lg:w-2/5
+  m-auto
 `}
+  padding-top:50vh
 `;
 
 const BubbleSpeech = styled.div`
+  position: relative;
   background-color: white;
   border-radius: 10px;
   padding: 15px;
   margin-bottom: 10px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-  &&::after {
+  &:after {
     border-top: 0px solid transparent;
     border-left: 10px solid transparent;
     border-right: 10px solid transparent;
     border-bottom: 10px solid white;
-    content: "";
+    content: '';
     position: absolute;
     top: -10px;
     left: 80px;
+  }
+  &:hover {
+    transform: scale(1.02);
   }
 `;
