72d99583da81f095cf29ee5532208b433ba93770
김태용 <amico741@naver.com>
2023-07-30 09:44:41 +0900

    feat: 랜덤 line을 가져오기 위한 repository추가

---
+++ backend/jansori/src/main/java/com/a606/jansori/domain/persona/repository/LineRepository.java
@@ -0,0 +1,14 @@
+package com.a606.jansori.domain.persona.repository;
+
+import com.a606.jansori.domain.persona.domain.Line;
+import com.a606.jansori.domain.persona.domain.Persona;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LineRepository extends JpaRepository<Line, Long> {
+
+  Long countLineByPersona(Persona persona);
+
+  List<Line> findLineByPersona(Persona persona, Pageable pageable);
+}
