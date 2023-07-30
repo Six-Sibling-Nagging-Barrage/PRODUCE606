c52ce56102599bfdf3f88eb19829025e44916771
김태용 <amico741@naver.com>
2023-07-30 16:25:16 +0900

    fix: 예외 코드 숫자 수정

--- backend/jansori/src/main/java/com/a606/jansori/domain/persona/exception/LineNotFoundException.java
+++ backend/jansori/src/main/java/com/a606/jansori/domain/persona/exception/LineNotFoundException.java
@@ -4,7 +4,7 @@ import com.a606.jansori.global.exception.NotFoundException;
 
 public class LineNotFoundException extends NotFoundException {
 
-  private static final String code = "850";
+  private static final String code = "950";
   private static final String message = "페르소나의 잔소리 대사를 찾을 수 없습니다.";
   public LineNotFoundException() {
     super(code, message);
