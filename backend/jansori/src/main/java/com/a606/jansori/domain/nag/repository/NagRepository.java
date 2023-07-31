eb67990f82216c50f581dd73f71bcd48117fe023
taeyong <amico741@naver.com>
2023-07-31 14:59:41 +0900

    feat: RandomGenerator 메서드 설명 및 메인페이지 랜덤 생성 기능 추가

--- backend/jansori/src/main/java/com/a606/jansori/domain/nag/repository/NagRepository.java
+++ backend/jansori/src/main/java/com/a606/jansori/domain/nag/repository/NagRepository.java
@@ -1,8 +1,13 @@
 package com.a606.jansori.domain.nag.repository;
 
 import com.a606.jansori.domain.nag.domain.Nag;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface NagRepository extends JpaRepository<Nag, Long> {
 
+  @Query(value = "select n from nag n order by n.likeCount desc ")
+  List<Nag> findByRandomNagsNotInTopRank(Pageable pageable);
 }
--- backend/jansori/src/main/java/com/a606/jansori/domain/nag/service/NagRandomGenerator.java
+++ backend/jansori/src/main/java/com/a606/jansori/domain/nag/service/NagRandomGenerator.java
@@ -2,6 +2,7 @@ package com.a606.jansori.domain.nag.service;
 
 import com.a606.jansori.domain.member.domain.Member;
 import com.a606.jansori.domain.nag.domain.Nag;
+import com.a606.jansori.domain.nag.repository.NagRepository;
 import com.a606.jansori.domain.persona.domain.Line;
 import com.a606.jansori.domain.persona.domain.Persona;
 import com.a606.jansori.domain.persona.exception.LineNotFoundException;
@@ -23,9 +24,18 @@ import org.springframework.stereotype.Component;
 public class NagRandomGenerator {
 
   private final NagTagRepository nagTagRepository;
+  private final NagRepository nagRepository;
   private final LineRepository lineRepository;
   private final RandomUtil randomUtil;
+  private final int MAX_PAGE_SIZE = 5;
 
+  /**
+   * 각 페르소나의 잔소리들 중에 랜덤으로 잔소리를 가져온다.
+   *
+   * @param persona 인스턴스
+   * @return Line
+   * @author 김태용
+   */
   public Line getRandomLineOfPersona(Persona persona) {
     Long count = lineRepository.countByPersona(persona);
 
@@ -36,9 +46,17 @@ public class NagRandomGenerator {
     return lineRepository.findLineByPersona(
             persona,
             PageRequest.of(randomUtil.generate(count), 1))
-            .get(0);
+        .get(0);
   }
 
+  /**
+   * todo의 잔소리를 랜덤으로 가져온다. 해당 잔소리는 member가 쓴 잔소리가 아니고, todo에 포함된 해시태그의 잔소리 를 가져온다.
+   *
+   * @param member   인스턴스
+   * @param todoTags todo가 가지고 있는 tags
+   * @return Nag
+   * @author 신현철
+   */
   public Nag getRandomNagWithTags(Member member, List<TodoTag> todoTags) {
 
     List<Tag> tags = todoTags.stream().map(TodoTag::getTag).collect(Collectors.toList());
@@ -49,5 +67,22 @@ public class NagRandomGenerator {
 
     return nagTagRepository.findByNag_MemberNotAndTagIn(member, tags,
         PageRequest.of(randomIndex, 1)).get(0).getNag();
+  }
+
+  /**
+   * 메인페이지에서 잔소리 좋아요 수의 TOP RANKING 5를 제외한 랜덤 잔소리들을 가져온다.
+   * 랜덤으로 선택된 페이지가 0일 경우 TOP RANKING 이기 때문에 + 1을 해줍니다.
+   * @return 메인페이지에서 사용될 잔소리 리스트
+   * @author 김태용
+   */
+  public List<Nag> getRandomNagsOfMainPage() {
+    Long count = (nagRepository.count() / MAX_PAGE_SIZE);
+    int randomIndex = randomUtil.generate(count);
+    if (randomIndex == 0) {
+      randomIndex++;
+    }
+
+    return nagRepository.findByRandomNagsNotInTopRank(
+        PageRequest.of(randomIndex, MAX_PAGE_SIZE));
   }
 }
