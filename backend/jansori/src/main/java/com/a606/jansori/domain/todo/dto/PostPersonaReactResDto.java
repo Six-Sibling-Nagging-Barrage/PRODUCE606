f124e47771e5514788ec51ea39382293c5cad98b
moonn6pence <shin421179@gmail.com>
2023-07-31 15:15:50 +0900

    refactor : DTO 수정
    - 기존 Feed 응답용 TodoPersonaDto에서 부모 자식간 필드 위치 변경

--- backend/jansori/src/main/java/com/a606/jansori/domain/todo/dto/PostPersonaReactResDto.java
+++ backend/jansori/src/main/java/com/a606/jansori/domain/todo/dto/PostPersonaReactResDto.java
@@ -1,5 +1,24 @@
 package com.a606.jansori.domain.todo.dto;
 
-public class PostPersonaReactResDto {
+import com.a606.jansori.domain.persona.domain.TodoPersona;
+import com.a606.jansori.domain.persona.dto.FeedTodoPersonaDetailDto;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
 
+@Getter
+@SuperBuilder
+public class PostPersonaReactResDto extends FeedTodoPersonaDetailDto {
+
+  private Boolean isFirstReaction;
+
+  public static PostPersonaReactResDto from(TodoPersona todoPersona) {
+
+    return PostPersonaReactResDto.builder()
+        .isFirstReaction(todoPersona.getLikeCount() == 1)
+        .todoPersonaId(todoPersona.getId())
+        .personaId(todoPersona.getPersona().getId())
+        .content(todoPersona.getLikeCount() == 1 ? todoPersona.getLine().getContent() : null)
+        .likeCount(todoPersona.getLikeCount())
+        .build();
+  }
 }
