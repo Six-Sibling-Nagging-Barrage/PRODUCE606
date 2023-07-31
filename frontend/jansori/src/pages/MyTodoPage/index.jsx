e134c9dff2e34c44aa65fb3693cee34d69f54668
lcj000107 <lcj000107@naver.com>
2023-07-31 10:10:51 +0900

    feat: 라우터 기능 추가

---
+++ frontend/jansori/src/pages/MyTodoPage/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyTodoPage = () => {
+  return <div>MyTodoPage</div>;
+};
+
+export default MyTodoPage;
---
+++ frontend/jansori/src/pages/NagBoxPage/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NagBoxPage = () => {
+  return <div>NagBoxPage</div>;
+};
+
+export default NagBoxPage;
