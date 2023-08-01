347e5452f64174be30852cabaeed6f42d43111ff
lcj000107 <lcj000107@naver.com>
2023-08-01 11:09:48 +0900

    feat: nagbox page에 CountBox 내용 부분 추가

--- frontend/jansori/src/pages/NagBoxPage/index.jsx
+++ frontend/jansori/src/pages/NagBoxPage/index.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React from 'react';
+import Background from '../../components/UI/Background';
+import CountBox from './components/CountBox';
+import tw, { styled } from 'twin.macro';
 
 const NagBoxPage = () => {
-  return <div>NagBoxPage</div>;
+  return (
+    <Background>
+      <CenteredContainer>
+        <CountBoxContainer>
+          <CountBox
+            startSentence={'현재 총'}
+            count={'127' + '개'}
+            endSentence={'잔소리가 작성되었습니다.'}
+          />
+          <CountBox
+            startSentence={'현재 총'}
+            count={'127' + '개'}
+            endSentence={'잔소리가 작성되었습니다.'}
+          />
+          <CountBox
+            startSentence={'현재 총'}
+            count={'127' + '개'}
+            endSentence={'잔소리가 작성되었습니다.'}
+          />
+        </CountBoxContainer>
+      </CenteredContainer>
+    </Background>
+  );
 };
 
 export default NagBoxPage;
+
+const CenteredContainer = styled.div`
+  ${tw`
+    flex
+    justify-center
+    items-center
+    h-full
+  `}
+`;
+
+const CountBoxContainer = styled.div`
+  ${tw`grid 
+  grid-cols-3
+  gap-4
+  w-9/12
+  w-fit
+  py-20`}
+`;
