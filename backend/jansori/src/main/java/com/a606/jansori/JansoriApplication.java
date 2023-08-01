fd1faa4eed8759019013603902f088a20a331ae0
허성백 <heosb123@hanmail.net>
2023-08-02 00:56:05 +0900

    infra: application.yml 내용 수정 및 경로 변경

--- backend/jansori/src/main/java/com/a606/jansori/JansoriApplication.java
+++ backend/jansori/src/main/java/com/a606/jansori/JansoriApplication.java
@@ -9,8 +9,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 public class JansoriApplication {
 
     public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:/application.yml, "
-            + "/app/config/springboot-jansori/application-release.yml";
+            + "/app/config/springboot-jansori/application.yml,";
+//            + "/app/config/springboot-jansori/application-release.yml";
 
     public static void main(String[] args) {
 //        SpringApplication.run(JansoriApplication.class, args);
