eaa3e08c839fd26ac04dfd348bce98c49a724f3b
이채정 <lcj000107@naver.com>
2023-08-02 01:14:26 +0900

    feat: 잔소리 랜덤으로 배치되는 부분에서 애니메이션 기능 추가

--- frontend/jansori/src/pages/MainPage/index.jsx
+++ frontend/jansori/src/pages/MainPage/index.jsx
@@ -1,16 +1,36 @@
 import React, { useState, useEffect } from 'react';
+import styled, { keyframes } from 'styled-components';
 import StartButton from './components/StartButton';
 import Background from '../../components/UI/Background';
 
 const MainPage = () => {
   // 더미데이터
-  const sentences = [
-    '네 미해결 Todo list 모아서 논문도 쓰겠다.',
-    '나는 자는 시간도 아까운데.. 넌 대체 뭐하고 있냐?',
-    '와 드디어 시작하려고? 난 또 너가 죽은 줄 알았잖아 ㅋㅋ',
-    '우와 방금 전에 먹고 또 먹는 거야? 신기해…',
-    '나같으면 빨리 하겠다 ㅋㅋ',
-    '노는게 그렇게 좋아?',
+  const nags = [
+    {
+      nagId: 1,
+      content: '니 코드가 개발새발인데 놀고싶어?',
+      createAt: null,
+    },
+    {
+      nagId: 2,
+      content: '일어나',
+      createAt: null,
+    },
+    {
+      nagId: 3,
+      content: '답없누',
+      createAt: null,
+    },
+    {
+      nagId: 4,
+      content: '잠이나 자',
+      createAt: null,
+    },
+    {
+      nagId: 5,
+      content: '답답하다~',
+      createAt: null,
+    },
   ];
 
   // 문장을 뿌려줄 좌표를 저장하는 상태
@@ -27,27 +47,51 @@ const MainPage = () => {
       return { x: RandomX, y: RandomY };
     };
 
-    const newPositions = sentences.map(() => generateRandomPosition());
+    const newPositions = nags.map(() => generateRandomPosition());
     setPositions(newPositions);
   }, []);
 
   return (
     <Background>
       <StartButton nagCount={'172032'} />
-      {sentences.map((sentence, index) => (
-        <div
-          key={index}
-          style={{
-            position: 'absolute',
-            top: positions[index]?.y || 100,
-            left: positions[index]?.x || 100,
-          }}
-        >
-          {sentence}
-        </div>
+      {nags.map((nag, index) => (
+        <Wrap>
+          <ContentContainer
+            key={index}
+            style={{
+              position: 'absolute',
+              top: positions[index]?.y || 100,
+              left: positions[index]?.x || 100,
+            }}
+          >
+            {nag.content}
+          </ContentContainer>
+        </Wrap>
       ))}
     </Background>
   );
 };
 
 export default MainPage;
+const motionAnimation = keyframes`
+  0% {
+    margin-top: 0px;
+  }
+  100% {
+    margin-top: 10px;
+  }
+`;
+const Wrap = styled.div`
+  text-align: center;
+  margin-top: 20px;
+`;
+
+const ContentContainer = styled.div`
+  animation: ${motionAnimation} 2s linear 0s infinite alternate;
+  margin-top: 0;
+  -webkit-animation: ${motionAnimation} 2s linear 0s infinite alternate;
+  margin-top: 0;
+  color: white;
+  font-weight: bold;
+  z-index: 0;
+`;
