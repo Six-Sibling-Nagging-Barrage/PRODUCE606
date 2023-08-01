6cb9dc7e932566ea025324f59400070ad46c5c03
허성백 <heosb123@hanmail.net>
2023-08-01 23:24:22 +0900

    infra: 배포를 위한 Application 파일 수정

--- backend/jansori/src/main/java/com/a606/jansori/JansoriApplication.java
+++ backend/jansori/src/main/java/com/a606/jansori/JansoriApplication.java
@@ -3,12 +3,20 @@ package com.a606.jansori;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class JansoriApplication {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "/app/config/springboot-jansori/application.yml,"
+            + "/app/config/sroom/real-application-release.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(JansoriApplication.class, args);
+//        SpringApplication.run(JansoriApplication.class, args);
+        new SpringApplicationBuilder(JansoriApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 
 }
