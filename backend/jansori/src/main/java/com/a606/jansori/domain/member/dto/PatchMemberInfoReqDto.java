4d7119d1451ce9cc271fc8a53a8069b4917a2f60
Bae Young Suk <bysuk01@hanyang.ac.kr>
2023-08-01 13:38:21 +0900

    feat: 정규표현식 추가 + GUEST ROLE 부여 로직 추가

--- backend/jansori/src/main/java/com/a606/jansori/domain/member/dto/PatchMemberInfoReqDto.java
+++ backend/jansori/src/main/java/com/a606/jansori/domain/member/dto/PatchMemberInfoReqDto.java
@@ -2,6 +2,7 @@ package com.a606.jansori.domain.member.dto;
 
 import java.util.List;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,7 +15,8 @@ import org.hibernate.validator.constraints.URL;
 public class PatchMemberInfoReqDto {
 
   @NotBlank(message = "닉네임이 공백입니다.")
-//    @Pattern(regexp =  )
+  @Pattern(regexp = "/(^\\s|\\s$|\\s{2,}|[^\\w가-힣])/", message = "한글, 영문, 숫자, _, 띄어쓰기만 사용할 수 있으며," +
+          "띄어쓰기를 연달아 사용하거나, 처음과 마지막에 사용할 수 없습니다.")
   @Length(min=1, max = 11, message = "닉네임은 1자 이상 11자 이하여야 합니다.")
   private String nickname;
 
