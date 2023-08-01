95f043d4ce0e6e12cb5ae2164c169f719e1bad4c
lcj000107 <lcj000107@naver.com>
2023-08-01 13:04:59 +0900

    feat: 잔소리 보내기 페이지 이동 버튼 추가

--- frontend/jansori/src/pages/NagBoxPage/index.jsx
+++ frontend/jansori/src/pages/NagBoxPage/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Background from '../../components/UI/Background';
 import CountBox from './components/CountBox';
+import { Link } from 'react-router-dom';
 import tw, { styled } from 'twin.macro';
 
 const NagBoxPage = () => {
@@ -38,6 +39,15 @@ const NagBoxPage = () => {
           ))}
         </CountBoxContainer>
       </CenteredContainer>
+
+      {/* 잔소리 버튼 */}
+      <SpeechBubble>
+        <Link to='/nag'>
+          <BubbleSpeech>
+            <p>잔소리 하러 가기</p>
+          </BubbleSpeech>
+        </Link>
+      </SpeechBubble>
     </Background>
   );
 };
@@ -60,4 +70,35 @@ const CountBoxContainer = styled.div`
   w-9/12
   w-fit
   py-20`}
+`;
+
+const SpeechBubble = styled.div`
+  ${tw`
+  w-2/3
+  sm:w-1/3
+  md:w-3/5
+  lg:w-2/5
+  mx-auto
+`}
+`;
+const BubbleSpeech = styled.div`
+  position: relative;
+  background-color: white;
+  border-radius: 10px;
+  padding: 15px;
+  margin-bottom: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  &:after {
+    border-top: 0px solid transparent;
+    border-left: 10px solid transparent;
+    border-right: 10px solid transparent;
+    border-bottom: 10px solid white;
+    content: '';
+    position: absolute;
+    top: -10px;
+    left: 80px;
+  }
+  &:hover {
+    transform: scale(1.02);
+  }
 `;
