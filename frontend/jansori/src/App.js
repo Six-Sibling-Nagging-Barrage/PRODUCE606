151da610dff6334cf5748a38fa9f55153d077276
이채정 <lcj000107@naver.com>
2023-08-01 22:29:06 +0900

    feat: 라우터에 characterinfo page 추가

--- frontend/jansori/src/App.js
+++ frontend/jansori/src/App.js
@@ -6,15 +6,14 @@ import FeedPage from './pages/FeedPage';
 // import MyTodoPage from './pages/MyTodoPage';
 import NagBoxPage from './pages/NagBoxPage';
 import ProfilePage from './pages/ProfilePage';
+import CharacterInfoPage from './pages/CharacterInfoPage';
 import NavBar from './components/NavBar/NavBar';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 function App() {
   return (
     <div className='App'>
-      <GoogleOAuthProvider
-        clientId={process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID}
-      >
+      <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID}>
         <NavBar />
       </GoogleOAuthProvider>
       <Routes>
@@ -25,6 +24,7 @@ function App() {
         <Route path='nagbox' element={<NagBoxPage />} />
         <Route path='profile' element={<ProfilePage />} />
         <Route path='signup' element={<SignUpPage />} />
+        <Route path='characterinfo' element={<CharacterInfoPage />} />
       </Routes>
     </div>
   );
