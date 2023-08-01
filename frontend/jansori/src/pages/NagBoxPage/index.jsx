91ae154bc34860e1b140904346ff7950477fa244
lcj000107 <lcj000107@naver.com>
2023-08-01 12:27:29 +0900

    refactor: 중복되는 부분 map으로 수정해서 중복 처리 제거

--- frontend/jansori/src/pages/NagBoxPage/index.jsx
+++ frontend/jansori/src/pages/NagBoxPage/index.jsx
@@ -4,25 +4,23 @@ import CountBox from './components/CountBox';
 import tw, { styled } from 'twin.macro';
 
 const NagBoxPage = () => {
+  const startSentences = ['현재 총', '지금까지', '현재 총'];
+  const counts = ['15698', '157', '586'];
+  const expressions = [' 개', ' 개', ' 명'];
+  const endSentences = ['잔소리가 작성되었습니다.', 'todo가 달성했습니다.', '사용하고 있습니다.'];
+
   return (
     <Background>
       <CenteredContainer>
         <CountBoxContainer>
-          <CountBox
-            startSentence={'현재 총'}
-            count={'127' + '개'}
-            endSentence={'잔소리가 작성되었습니다.'}
-          />
-          <CountBox
-            startSentence={'현재 총'}
-            count={'127' + '개'}
-            endSentence={'잔소리가 작성되었습니다.'}
-          />
-          <CountBox
-            startSentence={'현재 총'}
-            count={'127' + '개'}
-            endSentence={'잔소리가 작성되었습니다.'}
-          />
+          {startSentences.map((startSentence, index) => (
+            <CountBox
+              key={index}
+              startSentence={startSentence}
+              count={counts[index] + expressions[index]}
+              endSentence={endSentences[index]}
+            />
+          ))}
         </CountBoxContainer>
       </CenteredContainer>
     </Background>
