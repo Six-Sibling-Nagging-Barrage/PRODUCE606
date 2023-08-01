86894d59aa98adcf2b762cb77ab6614503ff4e96
허성백 <heosb123@hanmail.net>
2023-08-02 01:01:27 +0900

    infra: application.yml 추적 경로 방식 변경

--- backend/jansori/src/main/java/com/a606/jansori/JansoriApplication.java
+++ backend/jansori/src/main/java/com/a606/jansori/JansoriApplication.java
@@ -1,22 +1,18 @@
 package com.a606.jansori;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class JansoriApplication {
-
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "/app/config/springboot-jansori/application.yml,";
-//            + "/app/config/springboot-jansori/application-release.yml";
 
     public static void main(String[] args) {
 //        SpringApplication.run(JansoriApplication.class, args);
-        new SpringApplicationBuilder(JansoriApplication.class)
-                .properties(APPLICATION_LOCATIONS)
-                .run(args);
+        new SpringApplicationBuilder(JansoriApplication.class).properties(
+                        "spring.config.name:application",
+                        "spring.config.location:optional:/app/config/springboot-jansori/")
+                .build().run(args);
     }
 
 }
