b7654ab5284cd876e8a1bef207ecf3f8ff9cc298
김태용 <amico741@naver.com>
2023-08-02 10:32:42 +0900

    refactor: 멤버의 잔소리 좋아요 리팩토링 * refactor: 멤버의 잔소리 좋아요 여부에 대해 responseDto 생성 및 리팩토링
    * refactor: 잔소리 좋아요에 대한 리팩토링

---
+++ backend/jansori/src/main/java/com/a606/jansori/domain/nag/dto/PostNagLikeResDto.java
@@ -0,0 +1,22 @@
+package com.a606.jansori.domain.nag.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostNagLikeResDto {
+
+  private boolean liked;
+
+  public static PostNagLikeResDto ofStatusAboutMemberLikeNag(boolean liked) {
+    return PostNagLikeResDto.builder()
+        .liked(liked)
+        .build();
+  }
+}
