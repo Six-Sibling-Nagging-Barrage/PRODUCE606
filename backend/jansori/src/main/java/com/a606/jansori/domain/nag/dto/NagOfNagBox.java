a25b157f01eb25641a3f3853f9f09af954f3b38a
taeyong <amico741@naver.com>
2023-08-02 09:59:51 +0900

    feat: 잔소리의 작성자 정보를 포함하기 위해 NagMember 추가

--- backend/jansori/src/main/java/com/a606/jansori/domain/nag/dto/NagOfNagBox.java
+++ backend/jansori/src/main/java/com/a606/jansori/domain/nag/dto/NagOfNagBox.java
@@ -1,5 +1,7 @@
 package com.a606.jansori.domain.nag.dto;
 
+import com.a606.jansori.domain.nag.domain.Nag;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
@@ -7,7 +9,28 @@ import lombok.experimental.SuperBuilder;
 @Getter
 public class NagOfNagBox extends NagDto {
 
+  private String preview;
+
   private Boolean unlocked;
 
   private Integer likeCount;
+
+  @JsonProperty(value = "nagMember")
+  private NagMember nagMember;
+
+  public static NagOfNagBox fromNag(Nag nag) {
+    return NagOfNagBox.builder()
+        .nagId(nag.getId())
+        .content(nag.getContent())
+        .createAt(nag.getCreatedAt())
+        .preview(nag.getPreview())
+        .unlocked(false)
+        .likeCount(nag.getLikeCount())
+        .nagMember(NagMember.from(nag.getMember()))
+        .build();
+  }
+
+  public void changeUnlocked() {
+    this.unlocked = !this.unlocked;
+  }
 }
