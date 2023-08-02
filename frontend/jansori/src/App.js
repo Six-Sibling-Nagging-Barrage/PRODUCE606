29de1ba0d29405de42079bdc01a93b51b2b03f59
lcj000107 <lcj000107@naver.com>
2023-08-02 17:17:05 +0900

    fea: 부족했던 페이지 추가

--- frontend/jansori/src/App.js
+++ frontend/jansori/src/App.js
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import MainPage from './pages/MainPage';
-import SignUpPage from './pages/SignUpPage';
 import FeedPage from './pages/FeedPage';
-// import MyTodoPage from './pages/MyTodoPage';
+import SignUpPage from './pages/SignUpPage';
 import NagBoxPage from './pages/NagBoxPage';
+import NagPage from './pages/NagBoxPage';
 import ProfilePage from './pages/ProfilePage';
+import ProfileEditPage from './pages/ProfileEditPage';
 import CharacterInfoPage from './pages/CharacterInfoPage';
 import NavBar from './components/NavBar/NavBar';
 import { GoogleOAuthProvider } from '@react-oauth/google';
@@ -18,12 +19,12 @@ function App() {
       </GoogleOAuthProvider>
       <Routes>
         <Route path='/' element={<MainPage />} />
-        <Route path='signup' element={<SignUpPage />} />
         <Route path='feed' element={<FeedPage />} />
-        {/* <Route path='mytodo' element={<MyTodoPage />} /> */}
-        <Route path='nagbox' element={<NagBoxPage />} />
-        <Route path='profile' element={<ProfilePage />} />
         <Route path='signup' element={<SignUpPage />} />
+        <Route path='nagbox' element={<NagBoxPage />} />
+        <Route path='nag' elememnt={<NagPage />} />
+        <Route path='profile' element={<ProfilePage />} />
+        <Route path='profileedit' element={<ProfileEditPage />} />
         <Route path='characterinfo' element={<CharacterInfoPage />} />
       </Routes>
     </div>
