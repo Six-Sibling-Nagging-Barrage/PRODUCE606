39d41de9bcab80f8f973ea2bb6103c7d5bbaf256
taeyong <amico741@naver.com>
2023-08-02 10:21:30 +0900

    feat: 잔소리함 잔소리들을 위한 일급 컬렉션 추가

---
+++ backend/jansori/src/main/java/com/a606/jansori/domain/nag/domain/NagBox.java
@@ -0,0 +1,13 @@
+package com.a606.jansori.domain.nag.domain;
+
+import com.a606.jansori.domain.nag.dto.NagOfNagBox;
+import java.util.List;
+
+public class NagBox {
+
+  private List<NagOfNagBox> nags;
+
+  public NagBox(List<Nag> nags, List<NagUnlock> nagUnlocks) {
+    
+  }
+}
