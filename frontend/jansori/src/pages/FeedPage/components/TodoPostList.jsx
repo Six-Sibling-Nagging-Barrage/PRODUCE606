129da76fc14028cd14457c4babde99b3999d3af0
imhynni <omelas329@gmail.com>
2023-08-02 11:13:31 +0900

    refactor: 무한스크롤 2차 리팩토링

--- frontend/jansori/src/pages/FeedPage/components/TodoPostList.jsx
+++ frontend/jansori/src/pages/FeedPage/components/TodoPostList.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import TodoPost from './TodoPost';
 
 const TodoPostList = (props) => {
-  const { postList } = props;
+  const { todoPostList } = props;
 
   return (
     <div>
       <ul>
-        {postList &&
-          postList.map((post, index) => {
+        {todoPostList &&
+          todoPostList.map((post, index) => {
             return <TodoPost post={post} key={index} />; // TODO: key값 변경해줘야 함
           })}
       </ul>
