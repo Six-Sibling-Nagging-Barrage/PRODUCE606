0a1259977f3131a8e612dc06ed5d12f526474dcd
taeyong <amico741@naver.com>
2023-08-02 10:22:40 +0900

    feat: 잔소리 정보를 입력하기 위한 로직 추가

--- backend/jansori/src/main/java/com/a606/jansori/domain/nag/domain/NagBox.java
+++ backend/jansori/src/main/java/com/a606/jansori/domain/nag/domain/NagBox.java
@@ -2,12 +2,21 @@ package com.a606.jansori.domain.nag.domain;
 
 import com.a606.jansori.domain.nag.dto.NagOfNagBox;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class NagBox {
 
   private List<NagOfNagBox> nags;
 
   public NagBox(List<Nag> nags, List<NagUnlock> nagUnlocks) {
-    
+    makeTopRanksNags(nags);
+  }
+
+  public List<NagOfNagBox> getNags() {
+    return nags;
+  }
+
+  private void makeTopRanksNags(List<Nag> nags) {
+    this.nags = nags.stream().map(NagOfNagBox::fromNag).collect(Collectors.toList());
   }
 }
