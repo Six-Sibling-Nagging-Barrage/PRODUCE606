b8ccb269baec139704b7abb43a884dd79a5785d2
Bae Young Suk <bysuk01@hanyang.ac.kr>
2023-08-03 17:51:40 +0900

    feat: OAuthSuccessHandler 기능 추가

--- backend/jansori/src/main/java/com/a606/jansori/global/config/SecurityConfig.java
+++ backend/jansori/src/main/java/com/a606/jansori/global/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.a606.jansori.global.config;
 
+import com.a606.jansori.global.auth.service.OAuthAccessDeniedHandler;
 import com.a606.jansori.global.auth.service.OAuthService;
+import com.a606.jansori.global.auth.service.OAuthSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,48 +18,56 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 public class SecurityConfig {
 
-  private final OAuthService oAuthService;
+    private final OAuthSuccessHandler oAuthSuccessHandler;
+    private final OAuthService oAuthService;
+    private final OAuthAccessDeniedHandler oAuthAccessDeniedHandler;
 
-  @Bean
-  public CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration configuration = new CorsConfiguration();
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
 
-    configuration.addAllowedOriginPattern("*");
-    configuration.addAllowedHeader("*");
-    configuration.addAllowedMethod("*");
-    configuration.setAllowCredentials(true);
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
 
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration);
-    return source;
-  }
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-    http.httpBasic().disable()
-        .cors().configurationSource(corsConfigurationSource())
+        http.httpBasic()
+                .disable()
+                .cors().configurationSource(corsConfigurationSource())
 
-        .and()
-        .authorizeRequests()
-        .antMatchers("/register").hasRole("GUEST")
-        .anyRequest().permitAll()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/signup/**").hasRole("GUEST")
+                .anyRequest()
+                .authenticated()
+                .and().exceptionHandling()
+                .accessDeniedHandler(oAuthAccessDeniedHandler)
 
-        .and()
-        .csrf().disable()
-        .headers().frameOptions().disable()
+                .and()
+                .csrf().disable()
+                .headers().frameOptions().disable()
 
-        .and()
-        .logout().logoutSuccessUrl("/") //logout 요청시 홈으로 이동 - 기본 logout url = "/logout"
+                .and()
+                .logout().logoutSuccessUrl("/") //logout 요청시 홈으로 이동 - 기본 logout url = "/logout"
 
-        .and()
-        .oauth2Login()
-        .defaultSuccessUrl("/", true) //OAuth2 성공시 redirect
-        .userInfoEndpoint() //OAuth2 로그인 성공 이후 사용자 정보를 가져올 때 설정 담당
-        .userService(oAuthService);
+                .and()
+                .oauth2Login()
+//                .loginProcessingUrl("/oauth/authorization/google")
+//                .defaultSuccessUrl("/") //OAuth2 성공시 redirect
+                .successHandler(oAuthSuccessHandler)
+                .userInfoEndpoint() //OAuth2 로그인 성공 이후 사용자 정보를 가져올 때 설정 담당
+                .userService(oAuthService);
 
-    //OAuth2 로그인 성공 시, 작업을 진행할 MemberService
-    return http.build();
-  }
+        //OAuth2 로그인 성공 시, 작업을 진행할 MemberService
+        return http.build();
+    }
 
 }
