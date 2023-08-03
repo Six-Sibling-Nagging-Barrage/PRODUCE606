9694c1f3d064c93e16b1f1560650eb096c7c82e5
BaeYoungSuk <bysuk01@gmail.com>
2023-08-03 22:45:20 +0900

    feat: OAuthAccessDeniedHandler 추가

--- backend/jansori/src/main/java/com/a606/jansori/global/auth/service/OAuthSuccessHandler.java
+++ backend/jansori/src/main/java/com/a606/jansori/global/auth/service/OAuthSuccessHandler.java
@@ -32,11 +32,9 @@ public class OAuthSuccessHandler implements AuthenticationSuccessHandler {
 
         if(role == MemberRole.GUEST){
             uri = "http://i9a606.p.ssafy.io/signup";
-        } else if (savedRequest != null ){
+        } else if (savedRequest != null){
             uri = savedRequest.getRedirectUrl();
         }
-
-        log.info(uri);
 
         response.sendRedirect(uri);
 
