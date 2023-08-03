9230ca09ab2df6a220c71ef0b5f80fd7c1576778
lcj000107 <lcj000107@naver.com>
2023-08-03 13:40:57 +0900

    refactor: 라우터 이동하는 부분 Link로 수정

--- frontend/jansori/src/App.js
+++ frontend/jansori/src/App.js
@@ -4,7 +4,7 @@ import MainPage from './pages/MainPage';
 import FeedPage from './pages/FeedPage';
 import SignUpPage from './pages/SignUpPage';
 import NagBoxPage from './pages/NagBoxPage';
-import NagPage from './pages/NagBoxPage';
+import NagPage from './pages/NagPage';
 import ProfilePage from './pages/ProfilePage';
 import ProfileEditPage from './pages/ProfileEditPage';
 import CharacterInfoPage from './pages/CharacterInfoPage';
@@ -17,13 +17,13 @@ function App() {
       <NavBar />
       <Routes>
         <Route path='/' element={<MainPage />} />
-        <Route path='feed' element={<FeedPage />} />
-        <Route path='signup' element={<SignUpPage />} />
-        <Route path='nagbox' element={<NagBoxPage />} />
-        <Route path='nag' elememnt={<NagPage />} />
-        <Route path='profile' element={<ProfilePage />} />
-        <Route path='profileedit' element={<ProfileEditPage />} />
-        <Route path='characterinfo' element={<CharacterInfoPage />} />
+        <Route path='/feed' element={<FeedPage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/nagbox' element={<NagBoxPage />} />
+        <Route path='/nag' element={<NagPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+        <Route path='/profileedit' element={<ProfileEditPage />} />
+        <Route path='/characterinfo' element={<CharacterInfoPage />} />
       </Routes>
       <FloatingButton />
     </div>
