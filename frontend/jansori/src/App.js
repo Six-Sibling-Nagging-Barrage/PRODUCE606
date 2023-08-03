06f4548572bd5d2da26c2692cd4398e27d2abfe2
lcj000107 <lcj000107@naver.com>
2023-08-03 13:00:27 +0900

    feat: 잔소리보내기 이동하는 버튼 추가

--- frontend/jansori/src/App.js
+++ frontend/jansori/src/App.js
@@ -9,21 +9,23 @@ import ProfilePage from './pages/ProfilePage';
 import ProfileEditPage from './pages/ProfileEditPage';
 import CharacterInfoPage from './pages/CharacterInfoPage';
 import NavBar from './components/NavBar/NavBar';
+import FloatingButton from './components/FloatingButton/FloatingButton';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <NavBar />
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="feed" element={<FeedPage />} />
-        <Route path="signup" element={<SignUpPage />} />
-        <Route path="nagbox" element={<NagBoxPage />} />
-        <Route path="nag" elememnt={<NagPage />} />
-        <Route path="profile" element={<ProfilePage />} />
-        <Route path="profileedit" element={<ProfileEditPage />} />
-        <Route path="characterinfo" element={<CharacterInfoPage />} />
+        <Route path='/' element={<MainPage />} />
+        <Route path='feed' element={<FeedPage />} />
+        <Route path='signup' element={<SignUpPage />} />
+        <Route path='nagbox' element={<NagBoxPage />} />
+        <Route path='nag' elememnt={<NagPage />} />
+        <Route path='profile' element={<ProfilePage />} />
+        <Route path='profileedit' element={<ProfileEditPage />} />
+        <Route path='characterinfo' element={<CharacterInfoPage />} />
       </Routes>
+      <FloatingButton />
     </div>
   );
 }
