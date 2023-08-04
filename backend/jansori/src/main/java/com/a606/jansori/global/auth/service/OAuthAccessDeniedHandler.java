68390e1a0f7c39e14e8531eefbc6c1da59914b3e
Bae Young Suk <bysuk01@hanyang.ac.kr>
2023-08-04 17:59:37 +0900

    fix : CORS 에러 수정

--- backend/jansori/src/main/java/com/a606/jansori/global/auth/service/OAuthAccessDeniedHandler.java
+++ backend/jansori/src/main/java/com/a606/jansori/global/auth/service/OAuthAccessDeniedHandler.java
@@ -1,5 +1,6 @@
 package com.a606.jansori.global.auth.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -9,14 +10,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Slf4j
 @Component
 public class OAuthAccessDeniedHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response,
                        AccessDeniedException accessDeniedException)
-        throws IOException, ServletException {
+            throws IOException, ServletException {
 
-        response.sendRedirect(request.getContextPath());
+        log.info(request.getScheme());
+        log.info(request.getServerName());
+        log.info(String.valueOf(request.getServerPort()));
+
+        response.sendRedirect(
+                request.getScheme() + "://" +
+                        request.getServerName() + ":"
+                        + request.getServerPort());
 
     }
 }
--- backend/jansori/src/main/java/com/a606/jansori/global/auth/service/OAuthSuccessHandler.java
+++ backend/jansori/src/main/java/com/a606/jansori/global/auth/service/OAuthSuccessHandler.java
@@ -1,6 +1,12 @@
 package com.a606.jansori.global.auth.service;
 
 import com.a606.jansori.domain.member.domain.MemberRole;
+import com.a606.jansori.domain.member.dto.GetLoginSuccessResDto;
+import com.a606.jansori.global.auth.dto.PrincipalDetails;
+import com.a606.jansori.global.common.EnvelopeResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -16,25 +22,47 @@ import java.io.IOException;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class OAuthSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final ObjectMapper objectMapper;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response
-    , Authentication authentication) throws IOException, ServletException{
+            , Authentication authentication) throws IOException, ServletException {
 
         RequestCache requestCache = new HttpSessionRequestCache();
         SavedRequest savedRequest = requestCache.getRequest(request, response);
 
+        objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+
         PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
         MemberRole role = principalDetails.getRole();
+        Long memberId = principalDetails.getId();
+        Boolean isRegistered = role != MemberRole.GUEST;
 
-        String uri = "http://i9a606.p.ssafy.io/";
+//        String uri = "http://i9a606.p.ssafy.io/";
+        String uri = "http://localhost:3000/";
 
-        if(role == MemberRole.GUEST){
-            uri = "http://i9a606.p.ssafy.io/signup";
-        } else if (savedRequest != null){
+        if (role == MemberRole.GUEST) {
+            isRegistered = false;
+
+//            uri = "http://i9a606.p.ssafy.io/signup";
+            uri = "http://localhost:3000/signup";
+        } else if (savedRequest != null) {
             uri = savedRequest.getRedirectUrl();
         }
+
+//        String json = objectMapper.writeValueAsString(
+//                EnvelopeResponse.<GetLoginSuccessResDto>builder()
+//                        .data(GetLoginSuccessResDto.builder()
+//                                .id(memberId)
+//                                .isRegistered(isRegistered).
+//                                build()));
+//        response.setStatus(HttpServletResponse.SC_OK);
+//        response.setCharacterEncoding("utf-8");
+//        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+//        response.getWriter().write(json);
 
         response.sendRedirect(uri);
 
