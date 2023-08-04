6a4894f77758f57fae7bb58459489e37e1e06288
taeyong <amico741@naver.com>
2023-08-04 15:53:57 +0900

    style: TagFollow style 변경

--- backend/jansori/src/main/java/com/a606/jansori/domain/tag/domain/TagFollow.java
+++ backend/jansori/src/main/java/com/a606/jansori/domain/tag/domain/TagFollow.java
@@ -1,11 +1,17 @@
 package com.a606.jansori.domain.tag.domain;
 
 import com.a606.jansori.domain.member.domain.Member;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-
-import javax.persistence.*;
 import lombok.NoArgsConstructor;
 
 @Builder
@@ -15,16 +21,23 @@ import lombok.NoArgsConstructor;
 @Entity(name = "tag_follow")
 public class TagFollow {
 
-    @Id
-    @Column(name = "tag_follow_id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @Column(name = "tag_follow_id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
-    private Member member;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private Member member;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "tag_id")
-    private Tag tag;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "tag_id")
+  private Tag tag;
+
+  public static TagFollow fromTagAndMember(Tag tag, Member member) {
+    return TagFollow.builder()
+        .tag(tag)
+        .member(member)
+        .build();
+  }
 }
