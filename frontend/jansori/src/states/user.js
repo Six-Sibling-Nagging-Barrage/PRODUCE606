580381f3d38d925250f40a8dffa42190215d59ae
이채정 <lcj000107@naver.com>
2023-08-06 23:38:59 +0900

    feat: 유저 이미지, 닉네임 recoil 상태 설정

--- frontend/jansori/src/states/user.js
+++ frontend/jansori/src/states/user.js
@@ -1,5 +1,6 @@
 import { atom } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
+import profileImg from '../assets/profileImg.png';
 
 // localStorage에 저장되며, key 이름은 'recoil-persist'
 const { persistAtom } = recoilPersist();
@@ -7,5 +8,17 @@ const { persistAtom } = recoilPersist();
 export const isLogin = atom({
   key: 'isLogin',
   default: false,
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const profileImgData = atom({
+  key: 'image',
+  default: profileImg,
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const memberNameData = atom({
+  key: 'memberName',
+  default: '',
   effects_UNSTABLE: [persistAtom],
 });
