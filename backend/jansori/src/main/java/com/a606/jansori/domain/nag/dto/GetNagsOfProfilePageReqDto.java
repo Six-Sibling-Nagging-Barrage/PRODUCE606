3011888c7ac3e5c2cfae10b9ac9fef02c83e07ab
taeyong <amico741@naver.com>
2023-08-07 17:45:25 +0900

    feat: memberId를 통해 member의 작성 잔소리 조회를 위한 컨트롤러 추가

---
+++ backend/jansori/src/main/java/com/a606/jansori/domain/nag/dto/GetNagsOfProfilePageReqDto.java
@@ -0,0 +1,21 @@
+package com.a606.jansori.domain.nag.dto;
+
+import javax.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class GetNagsOfProfilePageReqDto {
+
+  private Long memberId;
+
+  private Long cursor;
+
+  @Positive(message = "유효하지 않은 페이지 크기입니다.")
+  private Integer size;
+
+  public GetNagsOfProfilePageReqDto() {
+    this.size = 10;
+  }
+}
