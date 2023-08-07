c995f956d8dd1440cef2419d6138a447d79845b7
lcj000107 <lcj000107@naver.com>
2023-08-07 11:30:07 +0900

    feat: navbar 클릭시 dropdown 되는 기능 추가

--- frontend/jansori/src/components/NavBar/DropdownProfileMenu.jsx
+++ frontend/jansori/src/components/NavBar/DropdownProfileMenu.jsx
@@ -17,16 +17,56 @@ const DropdownProfileMenu = () => {
     window.location.replace('/');
   };
 
-  return <DropdownProfileMenuContainer>DropdownProfileMenu</DropdownProfileMenuContainer>;
+  return (
+    <DropdownProfileMenuContainer>
+      <BackgroundContainer />
+      <DropdownMenuContent>
+        <ItemContainer>
+          <Avatar src={profileImg} alt='profile' />
+          <MemberName>{memberName}</MemberName>
+        </ItemContainer>
+        <ItemContainer>mypage</ItemContainer>
+        <ItemContainer>LOGOUT</ItemContainer>
+      </DropdownMenuContent>
+    </DropdownProfileMenuContainer>
+  );
 };
 
 export default DropdownProfileMenu;
 
 const DropdownProfileMenuContainer = styled.div`
-  ${tw`z-50 
+  ${tw`
   absolute 
-  right-3 
-  mt-2 
-  w-2/12 h-20 bg-red-500 rounded flex items-center justify-center text-white text-xl`};
-  top: calc(100% + 10px); /* Adjust the value as needed to control the distance */
+  right-10 
+  mt-3 
+  w-48`};
+`;
+
+const BackgroundContainer = styled.div`
+  ${tw`
+  z-40
+  bg-white
+  opacity-50
+  drop-shadow-lg
+  rounded-lg
+  absolute
+  w-full
+  h-full`}
+`;
+
+const DropdownMenuContent = styled.div`
+  ${tw`relative
+  z-50`};
+`;
+
+const ItemContainer = styled.div`
+  ${tw`flex justify-center items-center p-2`}
+`;
+
+const Avatar = styled.img`
+  ${tw`w-8 h-8 rounded-full border-2 border-violet-300 mr-5`}
+`;
+
+const MemberName = styled.span`
+  ${tw`text-base`}
 `;
