6517a5d9390b901a66a173255139ab0d164ed627
lcj000107 <lcj000107@naver.com>
2023-08-07 11:29:39 +0900

    design: floating button 디자인 수정

--- frontend/jansori/src/components/FloatingButton/FloatingButton.jsx
+++ frontend/jansori/src/components/FloatingButton/FloatingButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import tw, { styled } from 'twin.macro';
-import { AiOutlineAliwangwang } from 'react-icons/ai';
 import { useLocation, useNavigate } from 'react-router-dom'; // 이 부분에서 react-router-dom의 Link 컴포넌트를 가져옴
+import nagIcon from '../../assets/nag_icon.png';
 
 const FloatingButton = () => {
   const location = useLocation();
@@ -14,7 +14,7 @@ const FloatingButton = () => {
 
   return (
     <FloatingButtonContainer onClick={handleFloatingButton}>
-      <AiOutlineAliwangwang size='100' />
+      <img src={nagIcon} />
     </FloatingButtonContainer>
   );
 };
@@ -22,5 +22,12 @@ const FloatingButton = () => {
 export default FloatingButton;
 
 const FloatingButtonContainer = styled.div`
-  ${tw`fixed bottom-10 right-10 w-20 h-20 bg-red-500 rounded-full flex items-center justify-center text-white text-xl`};
+  ${tw`fixed bottom-10 right-10 w-16 h-16 rounded-full flex items-center justify-center text-white text-xl`};
+  background-color: rgba(255, 255, 255, 0.5);
+  padding: 15px;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+  & img {
+    filter: invert(30%) sepia(0%) saturate(0%) hue-rotate(50deg) brightness(104%) contrast(86%);
+  }
 `;
