01652828c00e117505293d6a8178d1a20f53fc9b
김태용 <amico741@naver.com>
2023-08-09 02:31:55 +0900

    chore: 불필요한 dependency제거 및 import 제거

--- backend/jansori/src/main/java/com/a606/jansori/domain/nag/repository/NagRepository.java
+++ backend/jansori/src/main/java/com/a606/jansori/domain/nag/repository/NagRepository.java
@@ -4,7 +4,6 @@ import com.a606.jansori.domain.member.domain.Member;
 import com.a606.jansori.domain.nag.domain.Nag;
 import com.a606.jansori.domain.tag.domain.Tag;
 import java.util.List;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
