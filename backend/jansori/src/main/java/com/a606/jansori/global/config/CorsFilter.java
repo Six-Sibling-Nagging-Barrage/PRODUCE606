10000651d034d655b6ca1161b81ed64d315c747d
BaeYoungSuk <bysuk01@gmail.com>
2023-08-08 19:30:50 +0900

    fix: cors 설정 변경

--- backend/jansori/src/main/java/com/a606/jansori/global/config/CorsFilter.java
+++ backend/jansori/src/main/java/com/a606/jansori/global/config/CorsFilter.java
@@ -28,7 +28,7 @@ public class CorsFilter implements Filter {
       throws IOException, ServletException {
     HttpServletResponse response = (HttpServletResponse) res;
     HttpServletRequest request = (HttpServletRequest) req;
-    response.setHeader("Access-Control-Allow-Origin", "http://i9a606.p.ssafy.io");
+    response.setHeader("Access-Control-Allow-Origin", "*");
     response.setHeader("Access-Control-Allow-Credentials", "true");
     response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
     response.setHeader("Access-Control-Max-Age", "3600");
