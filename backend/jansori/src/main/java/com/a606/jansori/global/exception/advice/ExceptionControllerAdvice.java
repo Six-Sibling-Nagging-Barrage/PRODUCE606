f301a9881f47cb7e61657e3800d74f57534e9101
moonn6pence <shin421179@gmail.com>
2023-08-08 14:40:29 +0900

    fix : BusinessExceptionHandler 추가

--- backend/jansori/src/main/java/com/a606/jansori/global/exception/advice/ExceptionControllerAdvice.java
+++ backend/jansori/src/main/java/com/a606/jansori/global/exception/advice/ExceptionControllerAdvice.java
@@ -2,6 +2,7 @@ package com.a606.jansori.global.exception.advice;
 
 import com.a606.jansori.global.common.EnvelopeResponse;
 import com.a606.jansori.global.exception.BadRequestException;
+import com.a606.jansori.global.exception.BusinessException;
 import com.a606.jansori.global.exception.ForbiddenException;
 import com.a606.jansori.global.exception.InternalException;
 import com.a606.jansori.global.exception.NotFoundException;
@@ -106,6 +107,19 @@ public class ExceptionControllerAdvice {
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   public EnvelopeResponse InternalException(InternalException e) {
 
+    log.error(e.getMessage());
+
+    return EnvelopeResponse.builder()
+        .code(e.getCode())
+        .message(e.getMessage())
+        .build();
+  }
+
+  @ExceptionHandler(BusinessException.class)
+  @ResponseStatus(HttpStatus.FORBIDDEN)
+  public EnvelopeResponse BusinessExceptionHandler(BusinessException e) {
+
+    e.printStackTrace();
     log.error(e.getMessage());
 
     return EnvelopeResponse.builder()
