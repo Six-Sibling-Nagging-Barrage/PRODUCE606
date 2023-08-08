74e3f20dbee75f7ac69d6252afb0fa50ef7f3554
lcj000107 <lcj000107@naver.com>
2023-08-08 20:31:45 +0900

    feat: jwt header에 넣는 부분 추가

--- frontend/jansori/src/pages/NagPage/index.jsx
+++ frontend/jansori/src/pages/NagPage/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Background from '../../components/UI/Background';
 import NagForm from './components/NagForm';
+import { addTokenToHeaders } from '../../apis/utils/authInstance';
+import { useRecoilValue } from 'recoil';
+import { memberTokenState } from '../../states/user';
 
 const NagPage = () => {
+  const jwtToken = useRecoilValue(memberTokenState);
+
+  addTokenToHeaders(jwtToken);
   return (
     <Background>
       <NagForm />
