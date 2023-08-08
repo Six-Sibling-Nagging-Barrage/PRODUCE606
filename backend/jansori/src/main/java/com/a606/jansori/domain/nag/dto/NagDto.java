47490bed42155d2306cb96cd1c6c72d336f4a539
김태용 <amico741@naver.com>
2023-08-09 02:31:04 +0900

    feat: Nag의 생성 일자를 위한 JsonFormat 추가

--- backend/jansori/src/main/java/com/a606/jansori/domain/nag/dto/NagDto.java
+++ backend/jansori/src/main/java/com/a606/jansori/domain/nag/dto/NagDto.java
@@ -1,6 +1,7 @@
 package com.a606.jansori.domain.nag.dto;
 
 import com.a606.jansori.domain.nag.domain.Nag;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
@@ -11,6 +12,8 @@ public class NagDto {
 
   private Long nagId;
   private String content;
+
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH-mm", timezone = "Asia/Seoul")
   private LocalDateTime createAt;
 
   public static NagDto from(Nag nag) {
