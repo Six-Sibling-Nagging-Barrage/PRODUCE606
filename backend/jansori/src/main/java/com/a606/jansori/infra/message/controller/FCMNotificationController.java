847f537e8f5df3f43908a4368a9780f16796d25f
Bae Young Suk <bysuk01@hanyang.ac.kr>
2023-08-09 18:00:16 +0900

    feat: 토큰 등록 메소드 추가

--- backend/jansori/src/main/java/com/a606/jansori/infra/message/controller/FCMNotificationController.java
+++ backend/jansori/src/main/java/com/a606/jansori/infra/message/controller/FCMNotificationController.java
@@ -1,19 +1,24 @@
 package com.a606.jansori.infra.message.controller;
 
-import com.a606.jansori.infra.message.dto.NotificationMessage;
+import com.a606.jansori.global.common.EnvelopeResponse;
+import com.a606.jansori.infra.message.dto.PostFcmTokenReqDto;
 import com.a606.jansori.infra.message.service.FirebaseMessagingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/fcm")
+@RequestMapping("/message")
 @RequiredArgsConstructor
 public class FCMNotificationController {
 
   private final FirebaseMessagingService firebaseMessagingService;
+
+  @PostMapping("/token/register")
+  private EnvelopeResponse<Void> registerToken(PostFcmTokenReqDto postFcmTokenReqDto) {
+    return EnvelopeResponse.<Void>builder().build();
+  }
 
 //  @PostMapping
 //  public String sendNotificationByToken(@RequestBody NotificationMessage notificationMessage){
