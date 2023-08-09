04d111b1816e8cf431f0fa562af81cad82595b7c
moonn6pence <shin421179@gmail.com>
2023-08-09 14:34:04 +0900

    hotfix : exception 경로 수정

--- backend/jansori/src/main/java/com/a606/jansori/domain/nag/exception/NagInvalidRequestException.java
+++ backend/jansori/src/main/java/com/a606/jansori/domain/nag/exception/NagInvalidRequestException.java
@@ -1,6 +1,6 @@
 package com.a606.jansori.domain.nag.exception;
 
-import com.a606.jansori.global.exception.BadRequestException;
+import com.a606.jansori.global.exception.domain.BadRequestException;
 
 public class NagInvalidRequestException extends BadRequestException  {
 
