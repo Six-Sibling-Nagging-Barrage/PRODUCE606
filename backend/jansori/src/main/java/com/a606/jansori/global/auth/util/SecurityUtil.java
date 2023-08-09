a6e61cdd90aa329ff7dd986ae433b32d2bcb4228
moonn6pence <shin421179@gmail.com>
2023-08-09 11:38:43 +0900

    refactor : 인증 관련 에러와 member 에러 구분

--- backend/jansori/src/main/java/com/a606/jansori/global/auth/util/SecurityUtil.java
+++ backend/jansori/src/main/java/com/a606/jansori/global/auth/util/SecurityUtil.java
@@ -3,26 +3,26 @@ package com.a606.jansori.global.auth.util;
 import com.a606.jansori.domain.member.domain.Member;
 import com.a606.jansori.domain.member.exception.MemberNotFoundException;
 import com.a606.jansori.domain.member.repository.MemberRepository;
-import com.a606.jansori.global.auth.exception.AuthMemberNotFoundException;
+import com.a606.jansori.global.exception.domain.UnauthorizedException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class SecurityUtil {
 
   private final MemberRepository memberRepository;
 
+  private static final String anonymousUser = "anonymousUser";
+
   public Member getCurrentMemberByToken() {
 
     final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-    if (authentication == null || authentication.getName() == null) {
-      throw new AuthMemberNotFoundException();
+    if (authentication == null || authentication.getName().equals(anonymousUser)) {
+      throw new UnauthorizedException();
     }
 
     return memberRepository.findMemberByEmail(authentication.getName())
@@ -33,24 +33,23 @@ public class SecurityUtil {
 
     final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-    if (authentication == null || authentication.getName() == null) {
+    if (authentication == null || authentication.getName().equals(anonymousUser)) {
       return null;
     }
 
-    return memberRepository.findMemberByEmail(authentication.getName()).orElse(null);
+    return memberRepository.findMemberByEmail(authentication.getName())
+        .orElseThrow(MemberNotFoundException::new);
   }
 
   public Long getCurrentMemberIdByToken() {
     final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-    if (authentication == null || authentication.getName() == null) {
-      throw new AuthMemberNotFoundException();
+    if (authentication == null || authentication.getName().equals(anonymousUser)) {
+      throw new UnauthorizedException();
     }
 
-    Member member = memberRepository.findMemberByEmail(authentication.getName())
-        .orElseThrow(MemberNotFoundException::new);
-    return member.getId();
-
+    return memberRepository.findMemberByEmail(authentication.getName())
+        .orElseThrow(MemberNotFoundException::new).getId();
   }
 
 
