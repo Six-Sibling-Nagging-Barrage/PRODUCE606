265943b84d1a737a5ed77252d3002e4914a9e8be
신현철 <shin421179@gmail.com>
2023-08-10 17:58:35 +0900

    feat : 리프레시 토큰 redis로 변경

--- backend/jansori/src/main/java/com/a606/jansori/infra/redis/util/BlackListUtil.java
+++ backend/jansori/src/main/java/com/a606/jansori/infra/redis/util/BlackListUtil.java
@@ -10,38 +10,11 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class BlackListUtil {
 
-  private final RedisTemplate<String, Object> redisTemplate;
-
   private final RedisTemplate<String, Object> blackListTemplate;
 
-  public void set(String key, Object o, long expiration) {
-    redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer(o.getClass()));
-    redisTemplate.opsForValue().set(key, o, expiration, TimeUnit.MILLISECONDS);
-  }
-
-  public Object get(String key) {
-    return redisTemplate.opsForValue().get(key);
-  }
-
-  public boolean delete(String key) {
-    return Boolean.TRUE.equals(redisTemplate.delete(key));
-  }
-
-  public boolean hasKey(String key) {
-    return Boolean.TRUE.equals(redisTemplate.hasKey(key));
-  }
-
   public void setBlackList(String key, Object o, long expiration) {
-    blackListTemplate.setValueSerializer(new Jackson2JsonRedisSerializer(o.getClass()));
+    blackListTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(o.getClass()));
     blackListTemplate.opsForValue().set(key, o, expiration, TimeUnit.MILLISECONDS);
-  }
-
-  public Object getBlackList(String key) {
-    return blackListTemplate.opsForValue().get(key);
-  }
-
-  public boolean deleteBlackList(String key) {
-    return Boolean.TRUE.equals(blackListTemplate.delete(key));
   }
 
   public boolean isBlackList(String key) {
