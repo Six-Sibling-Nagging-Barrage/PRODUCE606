29bd9c9a0d709e54a02e2687e4a2a2036210b0f2
lcj000107 <lcj000107@naver.com>
2023-08-10 14:02:02 +0900

    feat: 좋아요 토글 버튼 누를 때 변경되도록 설정

--- frontend/jansori/src/pages/NagBoxPage/components/NagRankingList.jsx
+++ frontend/jansori/src/pages/NagBoxPage/components/NagRankingList.jsx
@@ -1,34 +1,65 @@
 import React, { useState, useEffect } from 'react';
 import tw, { styled } from 'twin.macro';
 import NagRankingItem from './NagRankingItem';
-import { getNagRanking } from '../../../apis/api/nag';
+import { getNagRanking, updateLikeNag } from '../../../apis/api/nag';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 
 const NagRankingList = () => {
-  const [memberNagRankings, setMemberNagRankings] = useState([]);
+  const queryClient = useQueryClient();
 
-  useEffect(() => {
-    (async () => {
-      const data = await getNagRanking();
-      setMemberNagRankings(data.nags);
-    })();
-  }, []);
+  const fetchNagRankings = async () => {
+    const data = await getNagRanking();
+    return data;
+  };
+
+  const { data } = useQuery(['nagRankings'], fetchNagRankings);
+
+  const toggleLike = async (nagId) => {
+    const data = await updateLikeNag(nagId);
+    return data;
+  };
+
+  const updateLikeMutation = useMutation((nagId) => toggleLike(nagId), {
+    onMutate: async (nagId) => {
+      await queryClient.cancelQueries(['nagRankings']);
+      const prevNagRankings = queryClient.getQueryData(['nagRankings']);
+      queryClient.setQueryData(['nagRankings'], (oldData) => {
+        const newData = oldData?.nags.map((nagRanking) => {
+          if (nagRanking.nagId === nagId) {
+            return {
+              ...nagRanking,
+              likeCount: nagRanking.likeCount + 1,
+            };
+          }
+          return nagRanking;
+        });
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(['nagRankings']);
+    },
+    onError: (error, context) => {
+      // queryClient.setQueryData(['nagRankings'], context?.prevFeed);
+    },
+  });
 
   return (
     <NagRankingListWrap>
-      {memberNagRankings?.map((memberNagRanking, index) => {
-        const isOdd = (index + 1) % 2 !== 0;
-        return (
-          <NagRankingItemWrap isOdd={isOdd}>
-            <NagRankingItem
-              key={memberNagRanking.nagId}
-              id={memberNagRanking.nagId}
-              content={memberNagRanking.content}
-              likeCount={memberNagRanking.likeCount}
-              isOdd={isOdd}
-            />
-          </NagRankingItemWrap>
-        );
-      })}
+      {Array.isArray(data?.nags) &&
+        data?.nags.map((memberNagRanking, index) => {
+          const isOdd = (index + 1) % 2 !== 0;
+          return (
+            <NagRankingItemWrap isOdd={isOdd} key={memberNagRanking.nagId}>
+              <NagRankingItem
+                id={memberNagRanking.nagId}
+                content={memberNagRanking.content}
+                likeCount={memberNagRanking.likeCount}
+                isOdd={isOdd}
+                updateLikeMutation={updateLikeMutation.mutate}
+              />
+            </NagRankingItemWrap>
+          );
+        })}
     </NagRankingListWrap>
   );
 };
@@ -41,6 +72,8 @@ const NagRankingListWrap = styled.div`
   flex-col`}
 `;
 
-const NagRankingItemWrap = styled.div(({ isOdd }) => [
-  isOdd ? tw`mr-auto` : tw`ml-auto`,
-]);
+const NagRankingItemWrap = styled.div(
+  ({ isOdd }) => [isOdd ? tw`mr-auto` : tw`ml-auto`],
+  tw`w-3/5
+  my-2`
+);
