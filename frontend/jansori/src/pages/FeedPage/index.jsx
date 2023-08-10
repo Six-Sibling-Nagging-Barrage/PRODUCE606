7afe06e5f9510720f8e9e2d64c2bebb612d979e0
imhynni <omelas329@gmail.com>
2023-08-10 15:56:23 +0900

    feat: 피드에서 태그 검색 후 팔로우

--- frontend/jansori/src/pages/FeedPage/index.jsx
+++ frontend/jansori/src/pages/FeedPage/index.jsx
@@ -3,31 +3,69 @@ import Feed from './components/Feed';
 import { styled } from 'twin.macro';
 import { getFollowingFeed, getSpecificFeed } from '../../apis/api/todo';
 import HashTag from '../../components/HashTag/HashTag';
+import { memberIdState, memberRoleState } from '../../states/user';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { getFollowTagList, createFollowTag } from '../../apis/api/tag';
+import { followingTagsState } from '../../states/tag';
+import Button from '../../components/UI/Button';
 
 const FeedPage = () => {
   const [specificTag, setSpecificTag] = useState(-1);
   const [hashTagList, setHashTagList] = useState([]);
+
+  const memberId = useRecoilValue(memberIdState);
+  const [followingTags, setFollowingTags] = useRecoilState(followingTagsState);
+
+  useEffect(() => {
+    // 팔로우 중인 해시태그 api 호출
+    (async () => {
+      const data = await getFollowTagList(memberId);
+      setFollowingTags(data.tags);
+      console.log(data.tags);
+    })();
+  }, []);
 
   useEffect(() => {
     if (hashTagList.length === 0) return setSpecificTag(-1);
     setSpecificTag(hashTagList[0].tagId);
   }, [hashTagList]);
 
+  const handleFollowHashTag = async () => {
+    const data = await createFollowTag(specificTag);
+    console.log(data);
+  };
+
+  const isFollowingTag = (tagId) => {
+    console.log(followingTags);
+    return followingTags.some((item) => item.tagId === tagId);
+  };
+
   return (
     <FeedContainer>
-      <SearchDiv>
+      <Left>
         <Search>
-          <div>
-            <HashTag
-              editable={true}
-              hashTagLimit={1}
-              hashTagList={hashTagList}
-              setHashTagList={setHashTagList}
-              setSpecificTag={setSpecificTag}
-            />
-          </div>
+          {specificTag !== -1 && (
+            <Follow>
+              <Button
+                normal
+                onClick={handleFollowHashTag}
+                label={isFollowingTag(specificTag) ? '구독취소' : '구독하기'}
+              ></Button>
+            </Follow>
+          )}
+          <SearchBar>
+            <div>
+              <HashTag
+                editable={true}
+                hashTagLimit={1}
+                hashTagList={hashTagList}
+                setHashTagList={setHashTagList}
+                setSpecificTag={setSpecificTag}
+              />
+            </div>
+          </SearchBar>
         </Search>
-      </SearchDiv>
+      </Left>
       <Feed
         specificTag={specificTag}
         getFeedData={specificTag === -1 ? getFollowingFeed : getSpecificFeed}
@@ -43,16 +81,24 @@ const FeedContainer = styled.div`
   justify-content: center;
 `;
 
-const SearchDiv = styled.div`
+const Left = styled.div`
   position: relative;
   width: 30%;
 `;
 
 const Search = styled.div`
   margin: 0 auto;
+  display: flex;
   position: absolute;
   top: 10px;
   right: 10px;
+`;
+
+const Follow = styled.div`
+  margin: 0 20px;
+`;
+
+const SearchBar = styled.div`
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.3);
   border-radius: 5px;
   width: fit-content;
