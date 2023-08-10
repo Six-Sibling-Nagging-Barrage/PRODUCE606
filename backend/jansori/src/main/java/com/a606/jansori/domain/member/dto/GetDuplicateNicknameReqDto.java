4e488ef7d261cb9f65562b342cd4ebcdacdc4e61
허성백 <heosb123@hanmail.net>
2023-08-10 15:03:23 +0900

    fix: controller에 @Valid 어노테이션 추가
    * fix: dto에 setter 추가, 예외에 getter 삭제
    
    * fix: requestbody 삭제하고 query string으로 변경

--- backend/jansori/src/main/java/com/a606/jansori/domain/member/dto/GetDuplicateNicknameReqDto.java
+++ backend/jansori/src/main/java/com/a606/jansori/domain/member/dto/GetDuplicateNicknameReqDto.java
@@ -5,10 +5,12 @@ import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 public class GetDuplicateNicknameReqDto {
 
   @NotNull
