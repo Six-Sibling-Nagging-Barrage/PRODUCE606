35662c662d4c5ed9aa07dbeade5cb4cb3cf45f9c
이채정 <lcj000107@naver.com>
2023-08-13 19:41:42 +0900

    feat: 명예의 전당 페이지 좋아요, 잠금해제 기능 추가

--- frontend/jansori/src/pages/NagBoxPage/components/NagRankingItem.jsx
+++ frontend/jansori/src/pages/NagBoxPage/components/NagRankingItem.jsx
@@ -1,22 +1,61 @@
 import React from 'react';
-import tw, { styled } from 'twin.macro';
-import { FaThumbsUp } from 'react-icons/fa';
+import tw, { css, styled } from 'twin.macro';
+import { useRecoilValue } from 'recoil';
+import { memberIdState } from '../../../states/user';
+import likeIcon from '../../../assets/like_icon.avif';
+import lockIcon from '../../../assets/lock_icon.png';
+import { altImageUrl } from '../../../constants/image';
 import SpeechBubble from '../../../components/UI/SpeechBubble';
 
 const NagRankingItem = (props) => {
-  const { id, likeCount, content, isOdd, updateLikeMutation } = props;
+  const { nag, isodd, toggleLike, toggleUnlock } = props;
+  const memberId = useRecoilValue(memberIdState);
 
   const handleLikeClick = () => {
-    updateLikeMutation(id);
+    if (!nag.unlocked) return;
+    toggleLike({ nagId: nag.nagId });
+  };
+
+  const handleUnlockNag = async () => {
+    toggleUnlock({ nagId: nag.nagId });
+  };
+
+  const handleImgError = (e) => {
+    e.target.src = altImageUrl;
   };
 
   return (
-    <NagRankingItemContainer isOdd={isOdd}>
-      <SpeechBubble text={content} isOdd={isOdd} />
-      <LikeButton isOdd={isOdd} onClick={handleLikeClick}>
-        <FaThumbsUp /> {likeCount}
-        {likeCount}
-      </LikeButton>
+    <NagRankingItemContainer isodd={isodd}>
+      <SpeechBubble isodd={isodd}>
+        <SpeechBubbleWrap>
+          <Profile>
+            <ProfileImg src={nag.nagMember.imageUrl} onError={handleImgError} />
+            <NickName>{nag.nagMember.nickname}</NickName>
+          </Profile>
+          <CommentContentWrapper>
+            {memberId === nag.nagMember.memberId || nag.unlocked ? nag.content : nag.preview}
+          </CommentContentWrapper>
+          <ButtonGroup>
+            {memberId === nag.nagMember.memberId ||
+              (!nag.unlocked && (
+                <ButtonItem onClick={() => handleUnlockNag(nag.nagId)}>
+                  <UnlockImg src={lockIcon} />
+                </ButtonItem>
+              ))}
+            <ButtonItem onClick={() => handleLikeClick(nag.unlocked)}>
+              {nag.isLiked ? (
+                <LikeImg src={likeIcon} />
+              ) : (
+                <LikeImg
+                  src={likeIcon}
+                  filter='invert(99%) sepia(29%) saturate(0%) hue-rotate(229deg) brightness(112%) contrast(86%);'
+                />
+              )}
+              <LikeCount>{nag.likeCount}</LikeCount>
+            </ButtonItem>
+          </ButtonGroup>
+        </SpeechBubbleWrap>
+      </SpeechBubble>
     </NagRankingItemContainer>
   );
 };
@@ -31,7 +70,87 @@ w-4/5
 `}
 `;
 
-const LikeButton = styled.div(({ isOdd }) => [
-  tw`bg-white w-fit absolute z-50 `,
-  isOdd ? tw`-bottom-3 -right-3` : tw`-bottom-3 -left-3`,
-]);
+const SpeechBubbleWrap = styled.div``;
+
+const Profile = styled.div`
+  display: flex;
+  height: fit-content;
+  width: fit-content;
+  margin-left: 2vh;
+`;
+const ProfileImg = styled.img`
+  ${(props) =>
+    props.isMemberNag
+      ? css`
+          ${tw`w-7 rounded-full`}
+        `
+      : css`
+          ${tw`w-7 rounded-full`}
+        `}
+  margin: 0 auto;
+`;
+const NickName = styled.div`
+  height: fit-content;
+  width: fit-content;
+  margin-left: 10px;
+  margin-top: 5px;
+  font-size: 12px;
+`;
+
+const CommentContentWrapper = styled.div`
+  position: relative;
+  width: 70%;
+  text-align: left;
+  top: 50%;
+  margin-top: 1vh;
+  margin-left: 2vh;
+  font-size: 20px;
+  overflow-wrap: break-word;
+  font-weight: semi-bold;
+`;
+
+const ButtonGroup = styled.div`
+  position: absolute;
+  display: flex;
+  right: 15px;
+  height: 64px;
+  top: 50%;
+  transform: translateY(-50%);
+`;
+
+const ButtonItem = styled.button`
+  margin-right: 5px;
+`;
+
+const UnlockImg = styled.img`
+  filter: invert(61%) sepia(0%) saturate(0%) hue-rotate(163deg) brightness(91%) contrast(83%);
+  width: 40px;
+  padding: 8px;
+  &:hover {
+    animation: shake 0.2s ease-in-out infinite;
+  }
+
+  @keyframes shake {
+    0%,
+    100% {
+      transform: translateX(0);
+    }
+    25% {
+      transform: translateX(-3px) rotate(-5deg);
+    }
+    75% {
+      transform: translateX(3px) rotate(5deg);
+    }
+  }
+`;
+
+const LikeImg = styled.img`
+  filter: ${(props) => props.filter};
+  width: 20px;
+  padding-top: 5px;
+`;
+
+const LikeCount = styled.div`
+  margin-top: 2px;
+  font-size: 12px;
+`;
--- frontend/jansori/src/pages/NagBoxPage/components/NagRankingList.jsx
+++ frontend/jansori/src/pages/NagBoxPage/components/NagRankingList.jsx
@@ -1,11 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import tw, { styled } from 'twin.macro';
+import { useRecoilState } from 'recoil';
 import NagRankingItem from './NagRankingItem';
-import { getNagRanking, updateLikeNag } from '../../../apis/api/nag';
+import { getNagRanking, updateLikeNag, updateNagUnlock } from '../../../apis/api/nag';
+import { ticketState } from '../../../states/user';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import SnackBar from '../../../components/UI/SnackBar';
 
 const NagRankingList = () => {
   const queryClient = useQueryClient();
+  const [ticket, setTicket] = useRecoilState(ticketState);
+  const [showSnackBar, setShowSnackBar] = useState(false);
+  const [snackBarMessage, setSnackBarMessage] = useState('');
 
   const fetchNagRankings = async () => {
     const data = await getNagRanking();
@@ -14,8 +20,8 @@ const NagRankingList = () => {
 
   const { data } = useQuery(['nagRankings'], fetchNagRankings);
 
-  const toggleLike = async (nagId) => {
-    const data = await updateLikeNag(nagId);
+  const toggleLike = async (nag) => {
+    const data = await updateLikeNag(nag.nagId);
     return data;
   };
 
@@ -24,7 +30,50 @@ const NagRankingList = () => {
       await queryClient.cancelQueries(['nagRankings']);
       const prevNagRankings = queryClient.getQueryData(['nagRankings']);
       queryClient.setQueryData(['nagRankings'], (oldData) => {
-        console.log(oldData);
+        const newData = oldData?.nags.map((nagRanking) => {
+          if (nagRanking.nagId === nagId) {
+            return {
+              ...nagRanking,
+              isLiked: !nagRanking.isLiked,
+              likeCount: nagRanking.isLiked ? nagRanking.likeCount - 1 : nagRanking.likeCount + 1,
+            };
+          }
+          return nagRanking;
+        });
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(['nagRankings']);
+    },
+  });
+
+  const handleSnackBarClose = () => {
+    setShowSnackBar(false);
+    setSnackBarMessage('');
+  };
+
+  const toggleUnlock = async (nag) => {
+    if (ticket < 1) {
+      setSnackBarMessage(
+        '티켓이 부족해 잔소리를 열어볼 수 없어요! 잔소리를 작성하면 티켓을 얻을 수 있어요.'
+      );
+      return setShowSnackBar(true);
+    }
+
+    const data = await updateNagUnlock(nag.nagId);
+    if (data.code === '200') {
+      setTicket(data.data.ticketCount);
+      setSnackBarMessage('티켓 1개를 소모해 잔소리를 열었어요.');
+      setShowSnackBar(true);
+    }
+    return data;
+  };
+
+  const updateUnlockMutation = useMutation((nagId) => toggleUnlock(nagId), {
+    onMutate: async (nagId) => {
+      await queryClient.cancelQueries(['nagRankings']);
+      const prevNagRankings = queryClient.getQueryData(['nagRankings']);
+      queryClient.setQueryData(['nagRankings'], (oldData) => {
         const newData = oldData?.nags.map((nagRanking) => {
           if (nagRanking.nagId === nagId) {
             return {
@@ -45,19 +94,20 @@ const NagRankingList = () => {
     <NagRankingListWrap>
       {Array.isArray(data?.nags) &&
         data?.nags.map((memberNagRanking, index) => {
-          const isOdd = (index + 1) % 2 !== 0;
+          const isodd = (index + 1) % 2 !== 0;
           return (
-            <NagRankingItemWrap isOdd={isOdd} key={memberNagRanking.nagId}>
+            <NagRankingItemWrap key={memberNagRanking.nagId} isodd={isodd}>
               <NagRankingItem
-                id={memberNagRanking.nagId}
-                content={memberNagRanking.content}
-                likeCount={memberNagRanking.likeCount}
-                isOdd={isOdd}
-                updateLikeMutation={updateLikeMutation.mutate}
+                key={memberNagRanking.nagId}
+                nag={memberNagRanking}
+                isodd={isodd}
+                toggleLike={updateLikeMutation.mutate}
+                toggleUnlock={updateUnlockMutation.mutate}
               />
             </NagRankingItemWrap>
           );
         })}
+      {showSnackBar && <SnackBar message={snackBarMessage} onClose={handleSnackBarClose} />}
     </NagRankingListWrap>
   );
 };
@@ -71,7 +121,7 @@ const NagRankingListWrap = styled.div`
 `;
 
 const NagRankingItemWrap = styled.div(
-  ({ isOdd }) => [isOdd ? tw`mr-auto` : tw`ml-auto`],
-  tw`w-3/5
+  ({ isodd }) => [isodd ? tw`mr-auto` : tw`ml-auto`],
+  tw`w-full md:w-3/5
   my-2`
 );
