ecbf7a399bda72238770895f8bc65e3e77d6bf1d
허성백 <heosb123@hanmail.net>
2023-08-14 10:07:55 +0900

    refactor: SuperBuilder 어노테이션 삭제

--- backend/jansori/src/main/java/com/a606/jansori/domain/notification/domain/Notification.java
+++ backend/jansori/src/main/java/com/a606/jansori/domain/notification/domain/Notification.java
@@ -14,11 +14,12 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
-@SuperBuilder
+@Builder
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
