6273b08f9b73826036152933e9a9fa0da3dfa698
lcj000107 <lcj000107@naver.com>
2023-08-14 17:56:24 +0900

    feat: firebase 추가할 기능 추가

---
+++ frontend/jansori/public/firebase-messaging-sw.js
@@ -0,0 +1,15 @@
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+
+var firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREABSE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+};
+
+const firebaseapp = firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging(firebaseapp);
