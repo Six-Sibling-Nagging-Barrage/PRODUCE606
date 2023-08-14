f5ae76af40fcc2ec20687a4b529d6a91b93ec779
imhynni <omelas329@gmail.com>
2023-08-14 23:03:41 +0900

    design: 색 수정

--- frontend/jansori/src/constants/hashtag.js
+++ frontend/jansori/src/constants/hashtag.js
@@ -4,8 +4,8 @@ export const hashTagColor = [
     font: 'rgb(173, 0, 255)',
   },
   {
-    background: 'rgba(136, 118, 247, 0.3)',
-    font: 'rgb(136, 118, 247)',
+    background: 'rgba(47, 29, 255, 0.2)',
+    font: 'rgba(47, 29, 255, 0.7)',
   },
   {
     background: 'rgba(0,148,255,0.18)',
