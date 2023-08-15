654ba4f3a040a98c7cca64299251a37005a2502e
신현철 <shin421179@gmail.com>
2023-08-15 21:41:49 +0900

    feat : dummy 추가
    * feat : dummy 추가

--- backend/jansori/src/main/java/com/a606/jansori/dummy/NagWithNagTagDummy.java
+++ backend/jansori/src/main/java/com/a606/jansori/dummy/NagWithNagTagDummy.java
@@ -3,7 +3,9 @@ package com.a606.jansori.dummy;
 import com.a606.jansori.domain.member.domain.Member;
 import com.a606.jansori.domain.nag.domain.Nag;
 import com.a606.jansori.domain.nag.repository.NagRepository;
+import com.a606.jansori.domain.nag.util.PreviewUtil;
 import com.a606.jansori.domain.tag.domain.Tag;
+import com.a606.jansori.dummy.DummyProperty.NagDummy;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -15,23 +17,27 @@ import org.springframework.stereotype.Component;
 @Profile("local")
 public class NagWithNagTagDummy {
 
+  private final DummyProperty dummyProperty;
   private final NagRepository nagRepository;
-  private final List<Nag> nags = new ArrayList<>();
+  private final PreviewUtil previewUtil;
 
   public List<Nag> createNags(List<Member> members, List<Tag> tags) {
-    int start;
 
-    for (int i = 0; i < members.size(); i++) {
-      start = 0;
-      for (int j = 0; j < 20; j++) {
-        nags.add(
-            Nag.builder()
-                .member(members.get(i))
-                .tag(tags.get(start))
-                .preview("ㅈㅅㄹ" + start)
-                .content("잔소리" + start++)
-                .likeCount(i).build());
-      }
+    List<Nag> nags = new ArrayList<>();
+
+    List<NagDummy> nagDummies = dummyProperty.getNagDummies();
+    int memberSize = members.size();
+
+    for (NagDummy nagDummy : nagDummies) {
+      int randMemberIdx = (int) (Math.random() * memberSize);
+      nags.add(Nag.builder()
+          .member(members.get(randMemberIdx))
+          .tag(tags.stream().filter((tag) -> tag.getName().equals(nagDummy.getTag())).findFirst()
+              .orElse(null))
+          .preview(previewUtil.convertNagToPreview(nagDummy.getContent()))
+          .content(nagDummy.getContent())
+          .likeCount((int) (Math.random() * 1024))
+          .build());
     }
 
     return nagRepository.saveAll(nags);
--- backend/jansori/src/main/java/com/a606/jansori/dummy/TodoWithTodoTagDummy.java
+++ backend/jansori/src/main/java/com/a606/jansori/dummy/TodoWithTodoTagDummy.java
@@ -3,15 +3,18 @@ package com.a606.jansori.dummy;
 
 import com.a606.jansori.domain.member.domain.Member;
 import com.a606.jansori.domain.nag.domain.Nag;
+import com.a606.jansori.domain.nag.service.NagRandomGenerator;
 import com.a606.jansori.domain.tag.domain.Tag;
 import com.a606.jansori.domain.tag.domain.TodoTag;
 import com.a606.jansori.domain.tag.repository.TodoTagRepository;
 import com.a606.jansori.domain.todo.domain.Todo;
 import com.a606.jansori.domain.todo.repository.TodoRepository;
+import com.a606.jansori.dummy.DummyProperty.TodoDummy;
 import com.a606.jansori.global.util.RandomUtil;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -21,43 +24,56 @@ import org.springframework.stereotype.Component;
 @Profile("local")
 public class TodoWithTodoTagDummy {
 
+  private final DummyProperty dummyProperty;
   private final TodoRepository todoRepository;
-  private final TodoTagRepository todoTagRepository;
-
-  private final List<Todo> todos = new ArrayList<>();
-  private final List<TodoTag> todoTags = new ArrayList<>();
-
-  private final RandomUtil randomUtil;
+  private final List<Todo> allTodos = new ArrayList<>();
+  private final NagRandomGenerator nagRandomGenerator;
 
   //멤버는 todo 를 생성하고,
   // todo 는 해시태그를 최소 0개에서 최대3개를 가질 수 있고
   //todo 는 잔소리를 한개 가질 수 있음
-  public List<Todo> createTodosWithTodoTags(List<Member> members, List<Tag> tags, List<Nag> nags) {
-    int start = 0;
+  public List<Todo> createTodosWithTodoTags(List<Member> members, List<Tag> tags) {
 
-    for (int i = 30; i >= 0; i--) {
-      for (int j = 0; j < members.size(); j++) {
-        for (int k = 0; k < 2; k++) { // 멤버당 하루에 todo 2개씩
-          Todo todo = Todo.builder()
-              .member(members.get(j))
-              .content("오늘의 할일" + start++)
-              .finished(false)
-              .display(k == 1 ? false : true)
-              .todoAt(LocalDate.now().minusDays(i))
-              .nag(nags.get(randomUtil.generate((long) nags.size())))
-              .build();
+    List<TodoDummy> todoDummies = dummyProperty.getTodoDummies();
+    int memberSize = members.size();
 
-          todos.add(todo);
+    for (int i = 7; i >= 0; i--) {
+      for (TodoDummy todoDummy : todoDummies) {
+        int randMemberIdx = (int) (Math.random() * memberSize);
 
-          TodoTag todoTag = TodoTag.builder()
-              .tag(tags.get(randomUtil.generate((long) tags.size())))
-              .todo(todo).build();
+        Todo todo = Todo.builder()
+            .member(members.get(randMemberIdx))
+            .content(todoDummy.getContent())
+            .finished((int) (Math.random() * 2) > 1)
+            .display(true)
+            .todoAt(LocalDate.now().minusDays(i))
+            .build();
 
-          todoTags.add(todoTag);
-        }
+        List<TodoTag> todoTags = todoDummy.getTags().stream()
+            .map(tagString -> {
+              Tag tag = tags.stream()
+                  .filter(tag1 -> tag1.getName().equals(tagString))
+                  .findFirst()
+                  .orElse(null);
+
+              TodoTag todoTag = TodoTag.builder()
+                  .tag(tag)
+                  .todo(todo)
+                  .build();
+
+              todoTag.setTodo(todo);
+
+              return todoTag;
+
+            }).collect(Collectors.toList());
+
+        todo.setNag(nagRandomGenerator.getRandomNagWithTags(members.get(randMemberIdx), todoTags)
+            .orElse(null));
+
+        allTodos.add(todo);
       }
     }
-    todoTagRepository.saveAll(todoTags);
-    return todoRepository.saveAll(todos);
+
+    return todoRepository.saveAll(allTodos);
   }
 }
