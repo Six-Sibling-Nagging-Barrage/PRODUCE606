22192f1e2b7f94a92dbdc692236737a431e8b65a
taeyong <amico741@naver.com>
2023-08-16 16:03:33 +0900

    feat: 잔소리함 통계 데이터 업데이트 기능 추가

--- backend/jansori/src/main/java/com/a606/jansori/infra/redis/util/NagBoxStatisticsUtil.java
+++ backend/jansori/src/main/java/com/a606/jansori/infra/redis/util/NagBoxStatisticsUtil.java
@@ -33,10 +33,10 @@ public class NagBoxStatisticsUtil {
         getNagBoxStatisticsResDto.getTotalNagsCount());
 
     nagBoxStatistics.putAll(KEY, nagBoxStatisticsMap);
-    
+
     //TTL 설정
     redisTemplate.expire(KEY, Duration.ofDays(TTL));
-    
+
     return getNagBoxStatisticsResDto();
   }
 
@@ -50,5 +50,38 @@ public class NagBoxStatisticsUtil {
         (Long) redisTemplate.opsForHash().get(KEY, TOTAL_DONE_TODO_COUNT),
         (Long) redisTemplate.opsForHash().get(KEY, TOTAL_NAGS_COUNT)
     );
+  }
+
+  public void increaseTotalNagCount() {
+    Long nagCount = (Long) redisTemplate.opsForHash().get(KEY, TOTAL_NAGS_COUNT);
+
+    if (nagCount == null) {
+      return;
+    }
+
+    redisTemplate.opsForHash().put(KEY, TOTAL_NAGS_COUNT, nagCount + 1);
+  }
+
+  public void updateTotalDoneTodoCount(boolean update) {
+    Long todoDoneCount = (Long) redisTemplate.opsForHash().get(KEY, TOTAL_DONE_TODO_COUNT);
+
+    if (todoDoneCount == null) {
+      return;
+    }
+    if(update) {
+      redisTemplate.opsForHash().put(KEY, TOTAL_DONE_TODO_COUNT, todoDoneCount + 1);
+    } else {
+      redisTemplate.opsForHash().put(KEY, TOTAL_DONE_TODO_COUNT, todoDoneCount - 1);
+    }
+  }
+
+  public void increaseTotalMemberCount() {
+    Long memberCount = (Long) redisTemplate.opsForHash().get(KEY, TOTAL_MEMBER_COUNT);
+
+    if (memberCount == null) {
+      return;
+    }
+
+    redisTemplate.opsForHash().put(KEY, TOTAL_MEMBER_COUNT, memberCount + 1);
   }
 }
