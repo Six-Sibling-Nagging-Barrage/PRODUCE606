779e1ba7d66586f332559d77f5a179658b073833
lcj000107 <lcj000107@naver.com>
2023-08-16 14:21:01 +0900

    fix: 캐릭터 프로필 디자인 수정

--- frontend/jansori/src/pages/CharacterInfoPage/index.jsx
+++ frontend/jansori/src/pages/CharacterInfoPage/index.jsx
@@ -1,88 +1,77 @@
 import React, { useState } from 'react';
 import tw, { styled } from 'twin.macro';
 import { personas } from '../../constants/persona';
+import { altImageUrl } from '../../constants/image';
 import logoImg from '../../assets/jansori-logo-eating-removebg-preview.png';
-import { useImageErrorHandler } from '../../hooks/useImageErrorHandler';
-import FeedBackground from '../../components/UI/FeedBackground';
 
 const CharacterInfoPage = () => {
   const [personaIndex, setPersonaIndex] = useState(-1);
-
-  const handleImgError = useImageErrorHandler();
 
   const handlePersonaHover = (personaId) => {
     setPersonaIndex(personaId - 1);
   };
 
+  const handleImgError = (e) => {
+    e.target.src = altImageUrl;
+  };
+
   return (
-    <>
-      {/* <FeedBackground /> */}
-      <PageContainer>
-        {/* 페르소나 이미지 gif 확대 해서 보여지는 부분 */}
-        <PersonaGifContainer>
-          {personaIndex === -1 ? (
-            <LogoImg src={logoImg} />
-          ) : (
-            <PersonaBigProfile key={personas[personaIndex].id}>
-              <PersonaBigImg src={personas[personaIndex].gifUrl} onError={handleImgError} />
-            </PersonaBigProfile>
-          )}
-        </PersonaGifContainer>
-        {/* 페르소나 이미지 작게 출력되는 부분 */}
-        <PersonaSmallImgContainer personaId={personaIndex}>
-          {personas.map((persona) => {
-            return (
-              <PersonaProfile
-                personaId={personaIndex}
-                key={persona.id}
-                onMouseEnter={() => handlePersonaHover(persona.id)}
-              >
-                <MiniProfileBackground isSelected={personaIndex === persona.id - 1} />
-                <PersonaImg src={persona.imgUrl} onError={handleImgError} />
-              </PersonaProfile>
-            );
-          })}
-        </PersonaSmallImgContainer>
-        {/* 페르소나 설명 출력 */}
-        <PersonaDescriptionContainer>
-          {personaIndex === -1 ? (
-            <PersonaFirst>
-              캐릭터에 마우스를 올리면 캐릭터에 대한 설명을 보실 수 있어요..
-            </PersonaFirst>
-          ) : (
-            <PersonaBio>
-              <PersonaDetailName>{personas[personaIndex].name}</PersonaDetailName>
-              <PersonaDetailDesc>
-                <PersonaDetailDescBio>
-                  <PersonaDetailDescTitle>🧐 성격</PersonaDetailDescTitle>
-                  <PersonaDetailDescContent>{personas[personaIndex].bio}</PersonaDetailDescContent>
-                </PersonaDetailDescBio>
-                <PersonaDetailDescBehind>
-                  <PersonaDetailDescTitle>🙄 비하인드 스토리</PersonaDetailDescTitle>
-                  <PersonaDetailDescContent>
-                    {personas[personaIndex].behindStory}
-                  </PersonaDetailDescContent>
-                </PersonaDetailDescBehind>
-              </PersonaDetailDesc>
-            </PersonaBio>
-          )}
-        </PersonaDescriptionContainer>
-      </PageContainer>
-    </>
+    <PageContainer>
+      {/* 페르소나 이미지 gif 확대 해서 보여지는 부분 */}
+      <PersonaGifContainer>
+        {personaIndex === -1 ? (
+          <LogoImg src={logoImg} />
+        ) : (
+          <PersonaProfile key={personas[personaIndex].id}>
+            <PersonaBigImg src={personas[personaIndex].gifUrl} onError={handleImgError} />
+          </PersonaProfile>
+        )}
+      </PersonaGifContainer>
+      {/* 페르소나 이미지 작게 출력되는 부분 */}
+      <PersonaSmallImgContainer>
+        {personas.map((persona) => {
+          return (
+            <PersonaProfile key={persona.id} onMouseEnter={() => handlePersonaHover(persona.id)}>
+              <PersonaImg src={persona.imgUrl} onError={handleImgError} />
+            </PersonaProfile>
+          );
+        })}
+      </PersonaSmallImgContainer>
+      {/* 페르소나 설명 출력 */}
+      <PersonaDescriptionContainer>
+        {personaIndex === -1 ? (
+          <PersonaFirst>
+            캐릭터에 마우스를 올리면 캐릭터에 대한 설명을 보실 수 있어요..
+          </PersonaFirst>
+        ) : (
+          <div>
+            <PersonaDetailName>{personas[personaIndex].name}</PersonaDetailName>
+            <PersonaDetailDesc>
+              <PersonaDetailDescBio>
+                <PersonaDetailDescTitle>🧐 성격</PersonaDetailDescTitle>
+                <div>{personas[personaIndex].bio}</div>
+              </PersonaDetailDescBio>
+              <PersonaDetailDescBehind>
+                <PersonaDetailDescTitle>🙄 비하인드 스토리</PersonaDetailDescTitle>
+                <div>{personas[personaIndex].behindStory}</div>
+              </PersonaDetailDescBehind>
+            </PersonaDetailDesc>
+          </div>
+        )}
+      </PersonaDescriptionContainer>
+    </PageContainer>
   );
 };
 
 export default CharacterInfoPage;
 
 const PageContainer = styled.div`
-  margin-top: 20px;
+  margin-top: 15vh;
   align-items: center;
 `;
 
 const LogoImg = styled.img`
   height: 20vh;
-  margin-top: 10vh;
-  margin-bottom: 10vh;
 `;
 
 const PersonaGifContainer = styled.div`
@@ -98,8 +87,6 @@ const PersonaBigImg = styled.img`
 
 // 페르소나 이미지 작게 출력되는 부분
 const PersonaSmallImgContainer = styled.div`
-  position: relative;
-  z-index: 1;
   width: 40vw;
   display: grid;
   grid-template-columns: repeat(6, 1fr);
@@ -111,58 +98,32 @@ const PersonaSmallImgContainer = styled.div`
   }
 `;
 
-const PersonaBigProfile = styled.div`
-  display: flex;
-`;
-
 const PersonaProfile = styled.div`
   display: flex;
-  position: relative;
-`;
-
-const MiniProfileBackground = styled.div`
-  position: absolute;
-  z-index: 0;
-  width: 100%;
-  height: 100%;
-  background-color: ${({ isSelected }) => (isSelected ? '#eeeef5' : 'transparent')};
-  border-radius: ${({ isSelected }) => (isSelected ? '50%' : 'transparent')};
-  padding: 3vh;
-  top: 0;
-  left: 0;
-  margin: 0 auto;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  z-index: 5;
 `;
 
 const PersonaImg = styled.img`
   ${tw`w-20`}
-  z-index: 2;
-  margin: 0 auto;
 `;
 
 // 페르소나 설명 출력하는 부분
 const PersonaDescriptionContainer = styled.div`
   position: relative;
   top: -15px;
-  width: 60vw;
-  height: 28vh;
+  width: 70vw;
+  height: 30vh;
   margin: 0 auto;
   padding-top: 3vh;
   background-color: #eeeef5;
   border-radius: 20px;
-  box-shadow: 0 0 10px rgba(163, 163, 163, 0.2);
+  box-shadow: 0 0 10px rgba(163, 163, 163, 0.3);
   overflow-y: scroll;
   /* ( 크롬, 사파리, 오페라, 엣지 ) 동작 */
   &::-webkit-scrollbar {
     display: none;
   }
   scrollbar-width: none; /* 파이어폭스 */
-  @media (max-width: 768px) {
-    width: 80vw;
-    grid-template-columns: repeat(6, 1fr);
-  }
 `;
 
 const PersonaFirst = styled.div`
@@ -172,17 +133,13 @@ const PersonaFirst = styled.div`
   height: 90%;
 `;
 
-const PersonaBio = styled.div``;
-
 const PersonaDetailName = styled.div`
-  z-index: 1;
   font-weight: bold;
   font-size: 28px;
 `;
 
 const PersonaDetailDesc = styled.div`
-  z-index: 1;
-  width: 90%;
+  width: 85%;
   margin: 0 auto;
   margin-top: 2vh;
   text-align: left;
@@ -199,10 +156,4 @@ const PersonaDetailDescBehind = styled.div`
 const PersonaDetailDescTitle = styled.div`
   font-weight: 500;
   font-size: 18px;
-  margin-bottom: 1vh;
-`;
-
-const PersonaDetailDescContent = styled.div`
-  width : 90%';
-  margin: 0 auto;
 `;
