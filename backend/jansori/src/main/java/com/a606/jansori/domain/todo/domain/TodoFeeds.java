15ead58d89f3d5e1863cc3c16af6977f694d8b0f
김태용 <amico741@naver.com>
2023-08-17 11:07:50 +0900

    refactor: TodoFeed에서 NagLike 및 NagUnlock 관계 제거 및 리팩토링

---
+++ backend/jansori/src/main/java/com/a606/jansori/domain/todo/domain/TodoFeeds.java
@@ -0,0 +1,36 @@
+package com.a606.jansori.domain.todo.domain;
+
+import com.a606.jansori.domain.nag.domain.NagInteraction;
+import com.a606.jansori.domain.todo.dto.TodoFeedDto;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.Getter;
+
+@Getter
+public class TodoFeeds {
+
+  List<TodoFeedDto> todoFeeds;
+
+  public TodoFeeds(List<Todo> todos, List<NagInteraction> nagInteractions) {
+    makeTodoFeeds(todos);
+    matchNagUnlockWithLikeStatus(nagInteractions);
+  }
+
+  private void makeTodoFeeds(List<Todo> todos) {
+    todoFeeds = todos.stream()
+        .map(todo -> TodoFeedDto.from(todo, false, false))
+        .collect(Collectors.toList());
+  }
+
+  private void matchNagUnlockWithLikeStatus(List<NagInteraction> nagInteractions) {
+    nagInteractions.forEach(this::changeNagUnlockWithLikeStatus);
+  }
+
+  private void changeNagUnlockWithLikeStatus(NagInteraction nagInteraction) {
+    todoFeeds.forEach(todoFeed -> {
+      if (todoFeed.getFeedNagDto().getNagId().equals(nagInteraction.getNag().getId())) {
+        todoFeed.changeNagLikedAndUnlockStatus(nagInteraction);
+      }
+    });
+  }
+}
